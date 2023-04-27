829aa506f68460aab088f38aa0cc85d4af3ed35e
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-04-27 16:41:43 +0500

    Add User Entity

---
+++ MRA-Jobs/src/Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace MRA_Jobs.Domain.Entities;
+public class User
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Patronymic { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateTime BirthDay { get; set; }
+    public int RoleId { get; set; }
+}
