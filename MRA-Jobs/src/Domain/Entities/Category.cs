fb5724baedbd5dbd2da2948570c00e23ab871fe0
ummatkulovalisher <ummatkulovalisher@gmail.com>
2023-04-28 13:17:27 +0500

    CategoryConfigurationAdded

--- MRA-Jobs/src/Domain/Entities/Category.cs
+++ MRA-Jobs/src/Domain/Entities/Category.cs
@@ -9,4 +9,6 @@ namespace MRA_Jobs.Domain.Entities;
 public class Category:BaseEntity
 {
     public string Name { get; set; }
+    public ICollection<JobVacancy>? JobVacancies { get; set; }
+    public ICollection<EducationVacancy>? EducationVacancies { get; set; }
 }
---
+++ MRA-Jobs/src/Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MRA_Jobs.Domain.Entities;
+
+namespace MRA_Jobs.Infrastructure.Persistence.Configurations;
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+       builder.HasKey(x => x.Id);
+        builder.Property(s=>s.Id).ValueGeneratedOnAdd();
+        builder.HasMany(s=>s.JobVacancies).WithOne(s=> s.Category);
+        builder.HasMany(s => s.EducationVacancies).WithOne(s => s.Category);
+
+    }
+}
