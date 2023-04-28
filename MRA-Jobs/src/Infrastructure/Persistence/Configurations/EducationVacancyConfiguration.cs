f0cb675ad5e4a88b823ce103c8b7aea4e7cba7bc
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-04-28 13:13:27 +0500

    Update EducationVacancyConfig

--- MRA-Jobs/src/Infrastructure/Persistence/Configurations/EducationVacancyConfiguration.cs
+++ MRA-Jobs/src/Infrastructure/Persistence/Configurations/EducationVacancyConfiguration.cs
@@ -1,6 +1,6 @@
-﻿using MRA_Jobs.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MRA_Jobs.Domain.Entities;
 
 namespace MRA_Jobs.Infrastructure.Persistence.Configurations;
 
@@ -9,6 +9,10 @@ public class EducationVacancyConfiguration : IEntityTypeConfiguration<EducationV
     public void Configure(EntityTypeBuilder<EducationVacancy> builder)
     {
         builder.ToTable(nameof(Vacancy));
+
+        builder.HasKey(x => x.Id);
+
+
 
         builder.HasData(new EducationVacancy() { Id = 5, Title = "Training class", Description = "tersd", ShortDescription = "sad" });
     }
