67d7e1e1c864d175eba3fcc6ef36594792a192f3
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-04-28 14:00:11 +0300

    status Con

--- MRA-Jobs/src/Infrastructure/Persistence/Configurations/StatusConfiguration.cs
+++ MRA-Jobs/src/Infrastructure/Persistence/Configurations/StatusConfiguration.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MRA_Jobs.Domain.Entities;
 
 namespace MRA_Jobs.Infrastructure.Persistence.Configurations;
-public class StatusConfiguration
+public class StatusConfiguration : IEntityTypeConfiguration<Status>
 {
+    public void Configure(EntityTypeBuilder<Status> builder)
+    {
+       builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name)
+            .HasMaxLength(300);
+    }
 }
