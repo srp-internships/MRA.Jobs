10476a3df1d23acc0fe242ac2d5f24a11ed9040d
daler0022 <daler_olimov_0055@mail.ru>
2023-04-28 13:58:00 +0500

    create configuration class for Application

---
+++ MRA-Jobs/src/Infrastructure/Persistence/Configurations/ApplicantConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MRA_Jobs.Domain.Entities;
+
+namespace MRA_Jobs.Infrastructure.Persistence.Configurations;
+public class ApplicantConfiguration : IEntityTypeConfiguration<Applicant>
+{
+    public void Configure(EntityTypeBuilder<Applicant> builder)
+    {
+        builder.HasKey(a => a.Id);
+    }
+}
---
+++ MRA-Jobs/src/Infrastructure/Persistence/Configurations/ApplicationConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MRA_Jobs.Domain.Entities;
+
+namespace MRA_Jobs.Infrastructure.Persistence.Configurations;
+public class ApplicationConfiguration : IEntityTypeConfiguration<Application>
+{
+    public void Configure(EntityTypeBuilder<Application> builder)
+    {
+        builder.HasKey(a  => a.Id);
+
+        builder.Property(a => a.ApplicantCvPath)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        builder.Property(a => a.ApplicantAbout)
+            .HasMaxLength(3000)
+            .IsRequired();
+
+        builder.HasOne<Applicant>(a => a.Applicant)
+            .WithMany(at => at.Applications)
+            .HasForeignKey(a => a.ApplicantId);
+    }
+}
