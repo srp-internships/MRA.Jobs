2dda13fa32f4cf6ff78c8017572897c0c691578c
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-04-28 10:42:07 +0500

    Add Id prop to User and Update Configuration

--- MRA-Jobs/src/Infrastructure/Persistence/Configurations/UserConfiguration.cs
+++ MRA-Jobs/src/Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -9,17 +9,18 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.ToTable(nameof(User));
+        builder.HasKey(u => u.Id);
         builder.HasData(new List<User>
         {
             new User()
             {
-                RoleId = 1, 
+                RoleId = 1,
                 FirstName = "TestName",
                 LastName = "TestLastName",
                 Patronymic = "text",
                 BirthDay = DateTime.UtcNow,
-                Age = 25, 
-                Email = "test@gmail.com", 
+                Age = 25,
+                Email = "test@gmail.com",
                 Password = "test1234",
                 PhoneNumber = "+9920000000"
             },
