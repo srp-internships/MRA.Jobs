46c94e5f26e486a1b0dd220534e65251989759fa
ummatkulovalisher <ummatkulovalisher@gmail.com>
2023-04-28 14:06:28 +0500

    Category Interface created

---
+++ MRA-Jobs/src/Application/Common/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MRA_Jobs.Domain.Entities;
+
+namespace MRA_Jobs.Application.Common.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<List<ServiceResponse<Category>>> Get();
+        public Task<ServiceResponse<Category>> GetById(int id);
+        public Task<ServiceResponse<bool>> Delete(int id);
+        public Task<ServiceResponse<Category>>Update(Category category);
+        public Task<ServiceResponse<Category>> Add(Category category);
+    }
+}
