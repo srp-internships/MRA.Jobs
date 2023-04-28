69945e68569157cf8a0bbe94399c7e1153f83b94
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-04-28 14:01:04 +0300

    status c

--- MRA-Jobs/src/Infrastructure/Persistence/Configurations/StatusConfiguration.cs
+++ MRA-Jobs/src/Infrastructure/Persistence/Configurations/StatusConfiguration.cs
@@ -12,7 +12,7 @@ public class StatusConfiguration : IEntityTypeConfiguration<Status>
 {
     public void Configure(EntityTypeBuilder<Status> builder)
     {
-       builder.HasKey(x => x.Id);
+        builder.HasKey(x => x.Id);
         builder.Property(x => x.Name)
             .HasMaxLength(300);
     }
