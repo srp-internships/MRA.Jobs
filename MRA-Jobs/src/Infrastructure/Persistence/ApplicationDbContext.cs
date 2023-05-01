cce1eb853f4e48f58a27891eb84dad388f04bdac
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-05-01 16:24:45 +0500

    update ApplicationDbContext

--- MRA-Jobs/src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ MRA-Jobs/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-
+        Database.EnsureCreated();
     }
 
     public DbSet<JobVacancy> JobVacancies { get; private set; }
