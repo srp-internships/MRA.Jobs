c1b8c9a14846f5bc718605f0178cfefd4d78edbc
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-05-01 19:00:41 +0500

    Project RE-Creating and bug fixing

---
+++ MRA-Jobs/src/Infrastructure/Migrations/20230501133844_Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MRA_Jobs.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Note",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2023, 5, 1, 13, 38, 44, 829, DateTimeKind.Utc).AddTicks(3305));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "BirthDay",
+                value: new DateTime(2023, 5, 1, 13, 38, 44, 829, DateTimeKind.Utc).AddTicks(764));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Note",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2023, 5, 1, 11, 41, 11, 963, DateTimeKind.Utc).AddTicks(8259));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "BirthDay",
+                value: new DateTime(2023, 5, 1, 11, 41, 11, 963, DateTimeKind.Utc).AddTicks(6880));
+        }
+    }
+}
