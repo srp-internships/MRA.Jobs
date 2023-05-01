e8856ab7e4f4db27f57c32f9fa1b685d9f699cd2
Muhammadislom Ismatov <muhammadislom99@gmail.com>
2023-05-01 13:04:21 +0500

    global using MRA_Jobs.Application.Common.Models;

--- MRA-Jobs/src/Domain/Entities/Note.cs
+++ MRA-Jobs/src/Domain/Entities/Note.cs
@@ -2,8 +2,8 @@ namespace MRA_Jobs.Domain.Entities;
 
 public class Note : BaseEntity
 {
-    public int AplicationId  { get; set; }
+    public int AplicationId { get; set; }
     public string Description { get; set; }
     public DateTime Date { get; set; }
-    public  User User { get; set; }
+    public User User { get; set; }
 }
