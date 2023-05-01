15faeaf3b6b2e3ae2b549a743d7133d3e217d540
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-05-01 12:13:15 +0500

    Add IJovVacancyService

--- MRA-Jobs/src/Application/GlobalUsings.cs
+++ MRA-Jobs/src/Application/GlobalUsings.cs
@@ -1,4 +1,3 @@
-﻿global using MRA_Jobs.Application.Common.Models;
-global using MRA_Jobs.Application.Common.Interfaces;
-
+﻿global using MRA_Jobs.Application.Abstractions;
+global using MRA_Jobs.Domain.Entities;
 
