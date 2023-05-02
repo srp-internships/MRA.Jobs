d97040e30637ce689a7074bd0c493368f80f2c4b
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-05-02 09:27:59 +0300

    NoteConroller is created and name of project fixed

---
+++ MRA-Jobs/MRA-Jobs/Server/Controllers/NoteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MRA-Jobs.Server.Controllers
+{
+    [Route("api/[controller]")]
+[ApiController]
+public class NoteController : ControllerBase
+{
+}
+}
