5ff860b406519f03e5b3453a60fb7536a92b48b3
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-05-02 08:56:16 +0300

    NoteService ccreated

---
+++ MRA-Jobs/src/Infrastructure/Services/NoteService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA_Jobs.Infrastructure.Services;
+internal class NoteService
+{
+}
