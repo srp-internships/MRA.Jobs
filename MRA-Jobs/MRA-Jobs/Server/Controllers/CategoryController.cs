213e5ae65307f07a928cc5edea017f8705d31d1c
ummatkulovalisher <ummatkulovalisher@gmail.com>
2023-05-02 09:57:27 +0500

    CategoryControllerAdded

--- MRA-Jobs/MRA-Jobs/Server/Controllers/CategoryController.cs
+++ MRA-Jobs/MRA-Jobs/Server/Controllers/CategoryController.cs
@@ -4,8 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace MRA_Jobs.Server.Controllers
 {
     [Route("api/[controller]")]
-[ApiController]
-public class CategoryController : ControllerBase
-{
-}
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+    }
 }
