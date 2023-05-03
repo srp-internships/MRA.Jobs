36b40acffdaeb715fd296282f661935be51a497c
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-05-03 15:38:17 +0300

    note controller

---
+++ src/Infrastructure/Persistence/Migrations/20230503113331_Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MRA.Jobs.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Note : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ApplicationNote",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2023, 5, 3, 11, 33, 31, 793, DateTimeKind.Utc).AddTicks(3580));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ApplicationNote",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2023, 5, 2, 11, 6, 53, 299, DateTimeKind.Utc).AddTicks(8887));
+        }
+    }
+}
---
+++ src/WebUI/Controllers/ApplicationNoteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MRA.Jobs.Application.Contracts.AplicationNotes.Commands;
+
+namespace MRA.Jobs.Web.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class ApplicationNoteController : ApiControllerBase
+{
+    private readonly ILogger<OidcConfigurationController> _logger;
+
+    public ApplicationNoteController(ILogger<OidcConfigurationController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpPost]
+    public async Task<ActionResult<long>> Add(CreatAplicationNoteCommand noteCommand, CancellationToken cancellationToken)
+    {
+        return await Mediator.Send(noteCommand, cancellationToken);
+    }
+}
