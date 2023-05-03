86ba538f92cf3876dabb5a05ce2ecfa22e540fd2
ummatkulovalisher <ummatkulovalisher@gmail.com>
2023-05-03 15:21:06 +0500

    Category Get and GetByid Created

---
+++ src/Application.Contracts/VacancyCategories/Commands/CreateVacancyCategoryCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
+public class CreateVacancyCategoryCommand:IRequest<long>
+{
+    public string Name { get; set; }
+}
---
+++ src/WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MRA.Jobs.Application.Contracts.VacancyCategories.Queries;
+
+namespace MRA.Jobs.Web.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ApiControllerBase
+{
+    private readonly ILogger<OidcConfigurationController> _logger;
+
+    public CategoryController(ILogger<OidcConfigurationController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var categories =await Mediator.Send(new GetVacancyCategoriesQuery());
+        return Ok(categories);
+    }
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var category = Mediator.Send(new GetByIdVacancyCategoryQuery { Id = id });
+        return Ok(category);
+    }
+}
