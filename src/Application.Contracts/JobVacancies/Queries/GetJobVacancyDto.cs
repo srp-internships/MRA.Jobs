d8209f013d39d313b606dfacb6fc5280809a4173
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-05-03 14:27:01 +0500

    initial

---
+++ src/Application.Contracts/JobVacancies/Queries/GetJobVacancyDto.cs
@@ -0,0 +1,17 @@
+﻿using MRA.Jobs.Application.Common.Mappings;
+using MRA.Jobs.Domain.Entities;
+
+namespace MRA.Jobs.Application.Contracts.JobVacancies.Queries;
+public class GetJobVacancyDto : IMapFrom<JobVacancy>
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string ShortDescription { get; set; }
+    public string Description { get; set; }
+    public DateTime PublishDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public long CategoryId { get; set; }
+    public int RequiredYearOfExperience { get; set; }
+    public decimal Salary { get; set; }
+    public int WorkSchedule { get; set; }
+}
---
+++ src/Application.Contracts/JobVacancies/Queries/GetJobVacancyQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace MRA.Jobs.Application.Contracts.JobVacancies.Queries;
+public class GetJobVacancyQuery : IRequest<GetJobVacancyDto>
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string ShortDescription { get; set; }
+    public string Description { get; set; }
+    public DateTime PublishDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public long CategoryId { get; set; }
+    public int RequiredYearOfExperience { get; set; }
+    public decimal Salary { get; set; }
+    public int WorkSchedule { get; set; }
+}
