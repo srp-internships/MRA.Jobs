88280ccb5e01575e4c68a7059a62340dd89ebc8d
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-05-03 13:48:10 +0300

    ApplicationNote create operation created

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -7,6 +7,7 @@ public interface IApplicationDbContext
 {
     DbSet<JobVacancy> JobVacancies { get; }
     DbSet<VacancyCategory> Categories { get; }
+    DbSet<ApplicationNote> Notes { get; }
     DbSet<Applicant> Applicants { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
