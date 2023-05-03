45bf9dc4a6cca7c0183a77f2584b4b2451776273
ummatkulovalisher <ummatkulovalisher@gmail.com>
2023-05-03 09:46:23 +0500

    Create and Update

---
+++ Application.Contracts/VacancyCategories/Commands/UpdateVacancyCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
+public class UpdateVacancyCategoryCommand
+{
+    public string Name { get; set; }
+}
