dbf40cce2471bc8d9fdf2884d4bc0269f8c7e7aa
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-05-04 15:10:41 +0300

    task 25 is completed

---
+++ src/Domain/Entities/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.Jobs.Domain.Entities;
+public class Test : BaseAuditableEntity
+{
+    public string Description { get; set; }
+    public string Duration { get; set; }
+    public long NumberOfQuestion { get; set; }
+    public string Title { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public long VacancyId { get; set; }
+    public long PassingScore { get; set; }
+    public string TestResults { get; set; }
+    public Vacancy Vacancy { get; set; }
+
+}
---
+++ src/Domain/Entities/TestResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.Jobs.Domain.Entities
+{
+    public class TestResult : BaseAuditableEntity
+    {
+        public long ApplicationId { get; set; }
+        public DateTime CompletedAt { get; set; }
+        public long Id { get; set; }
+        public bool Passed { get; set; }
+        public long Score { get; set; }
+        public long TestId { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Test Test { get; set; }
+        public Application Application { get; set; }
+
+
+    }
+}
