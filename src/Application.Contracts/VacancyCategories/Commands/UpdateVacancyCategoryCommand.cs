fafd1bcfe799aae7b7c6669f168465ee8cad2cb3
Mahmud Sharipov <mahmud@silkroadprofessionals.com>
2023-05-05 19:57:36 +0500

    fix problems

--- src/Application.Contracts/VacancyCategories/Commands/UpdateVacancyCategoryCommand.cs
+++ src/Application.Contracts/VacancyCategories/Commands/UpdateVacancyCategoryCommand.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 
 namespace MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
-public class UpdateVacancyCategoryCommand:IRequest<long>
+
+public class UpdateVacancyCategoryCommand : IRequest<long>
 {
     public long Id { get; set; }
     public string Name { get; set; }
