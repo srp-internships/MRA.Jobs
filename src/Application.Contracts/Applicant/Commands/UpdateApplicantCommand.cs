0225fc1f636ab7aa687e4e814932fae8096fbd93
nkomiljon <komiljonnajmitdinov99@gmail.com>
2023-05-10 15:48:46 +0500

    Updated UpdateApplicantCommand class

--- src/Application.Contracts/Applicant/Commands/UpdateApplicantCommand.cs
+++ src/Application.Contracts/Applicant/Commands/UpdateApplicantCommand.cs
@@ -5,4 +5,9 @@ namespace MRA.Jobs.Application.Contracts.Applicant.Commands;
 public class UpdateApplicantCommand : IRequest<Guid>
 {
     public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBrith { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
 }
