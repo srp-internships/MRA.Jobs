9ca04192019375db353c2b352972276e9b7843c0
nkomiljon <komiljonnajmitdinov99@gmail.com>
2023-05-10 15:52:57 +0500

    Update UpdateApplicantCommandValidator class

--- src/Application/Features/Applicant/Command/UpdateApplicant/UpdateApplicantCommandValidator.cs
+++ src/Application/Features/Applicant/Command/UpdateApplicant/UpdateApplicantCommandValidator.cs
@@ -1,6 +1,16 @@
-﻿namespace MRA.Jobs.Application.Features.Applicant.Command.UpdateApplicant;
+﻿using MRA.Jobs.Application.Contracts.Applicant.Commands;
 
-public class UpdateApplicantCommandValidator
+namespace MRA.Jobs.Application.Features.Applicant.Command.UpdateApplicant;
+
+public class UpdateApplicantCommandValidator : AbstractValidator<UpdateApplicantCommand>
 {
-    
+    public UpdateApplicantCommandValidator()
+    {
+        RuleFor(a => a.Id).NotEmpty();
+        RuleFor(a => a.LastName).NotEmpty();
+        RuleFor(a => a.FirstName).NotEmpty();
+        RuleFor(a => a.DateOfBrith).NotEmpty();
+        RuleFor(a => a.Email).NotEmpty();
+        RuleFor(a => a.PhoneNumber).NotEmpty();
+    }
 }
