0c0e3418b0f4df5e0fc1027b8be78215a43f6bc7
nkomiljon <komiljonnajmitdinov99@gmail.com>
2023-05-11 09:17:27 +0500

    Created Queries for Applicant

---
+++ src/Application/Features/Applicant/Query/GetApplicantById/GetApplicantByIdQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using MRA.Jobs.Application.Contracts.Applicant.Queries;
+
+namespace MRA.Jobs.Application.Features.Applicant.Query.GetApplicantById;
+
+public class GetApplicantByIdQueryValidator : AbstractValidator<GetApplicantByIdQuery>
+{
+    public GetApplicantByIdQueryValidator()
+    {
+        RuleFor(a => a.Id).NotEmpty();
+    }
+}
