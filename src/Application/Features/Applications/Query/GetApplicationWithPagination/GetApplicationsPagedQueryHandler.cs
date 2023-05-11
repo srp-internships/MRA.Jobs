165f297e959321355181477e994fa2f5c4a2734e
daler0022 <daler_olimov_0055@mail.ru>
2023-05-11 10:34:04 +0500

    change application crud

---
+++ src/Application/Features/Applications/Query/GetApplicationWithPagination/GetApplicationsPagedQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MRA.Jobs.Application.Contracts.Applications.Responses;
+using MRA.Jobs.Application.Contracts.Common;
+using MRA.Jobs.Infrastructure;
+
+namespace MRA.Jobs.Application.Features.Applications.Query.GetApplicationWithPagination;
+public class GetApplicationsPagedQueryHandler : IRequestHandler<PaggedListQuery<ApplicationListDTO>, PaggedList<ApplicationListDTO>>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IApplicationSieveProcessor _sieveProcessor;
+    private readonly IMapper _mapper;
+
+    public GetApplicationsPagedQueryHandler(IApplicationDbContext dbContext, IApplicationSieveProcessor sieveProcessor, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _sieveProcessor = sieveProcessor;
+        _mapper = mapper;
+    }
+
+    public Task<PaggedList<ApplicationListDTO>> Handle(PaggedListQuery<ApplicationListDTO> request, CancellationToken cancellationToken)
+    {
+        var result = _sieveProcessor.ApplyAdnGetPaggedList(request, _dbContext.Applications.AsNoTracking(), _mapper.Map<ApplicationListDTO>);
+        return Task.FromResult(result);
+    }
+}
