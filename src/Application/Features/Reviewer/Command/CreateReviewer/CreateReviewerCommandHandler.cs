e20591ad5214280a484b7987a1d2fae5e6c0992a
nkomiljon <komiljonnajmitdinov99@gmail.com>
2023-05-12 11:03:24 +0500

    Create CRUD for Reviewer

---
+++ src/Application/Features/Reviewer/Command/CreateReviewer/CreateReviewerCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MRA.Jobs.Application.Contracts.Reviewer.Command;
+
+namespace MRA.Jobs.Application.Features.Reviewer.Command.CreateReviewer;
+using Domain.Entities;
+
+public class CreateReviewerCommandHandler : IRequestHandler<CreateReviewerCommand, Guid>
+{
+    private readonly IMapper _mapper;
+    private readonly IApplicationDbContext _context;
+
+    public CreateReviewerCommandHandler(IMapper mapper, IApplicationDbContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+    public async Task<Guid> Handle(CreateReviewerCommand request, CancellationToken cancellationToken)
+    {
+        var reviewer = _mapper.Map<Reviewer>(request);
+        await _context.Reviewers.AddAsync(reviewer, cancellationToken);
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return reviewer.Id;
+    }
+}
---
+++ src/Application/Features/Reviewer/Command/UpdateReviewer/UpdateReviewerCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using MRA.Jobs.Application.Contracts.Reviewer.Command;
+
+namespace MRA.Jobs.Application.Features.Reviewer.Command.UpdateReviewer;
+
+public class UpdateReviewerCommandValidator : AbstractValidator<UpdateReviewerCommand>
+{
+    public UpdateReviewerCommandValidator()
+    {
+        RuleFor(r => r.Id).NotEmpty();
+    }
+}
