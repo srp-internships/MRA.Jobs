d06db73d916d041a9e123a136bbb4de5a3f76f82
nkomiljon <komiljonnajmitdinov99@gmail.com>
2023-05-12 11:03:24 +0500

    Create CRUD for Reviewer

---
+++ src/Application/Features/Reviewer/Query/GetAllReviewer/GetAllReviewerCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MRA.Jobs.Application.Contracts.Reviewer.Queries;
+using MRA.Jobs.Application.Contracts.Reviewer.Response;
+
+namespace MRA.Jobs.Application.Features.Reviewer.Query.GetAllReviewer;
+
+public class GetAllReviewerCommandHandler : IRequestHandler<GetAllReviewerQuery, ReviewerListDTO>
+{
+    public GetAllReviewerCommandHandler()
+    {
+        
+    }
+    
+    public Task<ReviewerListDTO> Handle(GetAllReviewerQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
