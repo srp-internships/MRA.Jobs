bc3e4f3dce6e0161281d91de3106b4a406ab8e4d
Muhammadislom Ismatov <muhammadislom99@gmail.com>
2023-05-12 16:29:50 +0500

    VacancyCategoryController

---
+++ src/Application.Contracts/VacancyCategories/Queries/GetAllVacancyCategoryQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using MRA.Jobs.Application.Contracts.VacancyCategories.Responces;
+
+namespace MRA.Jobs.Application.Contracts.VacancyCategories.Queries;
+
+public class GetAllVacancyCategoryQuery : IRequest<List<VacancyCategoryListDTO>>
+{
+}
--- src/WebUI/Controllers/CategoryController.cs
+++ src/WebUI/Controllers/CategoryController.cs
@@ -1,30 +1,56 @@
-﻿
-using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using MRA.Jobs.Application.Contracts.Common;
+using MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
 using MRA.Jobs.Application.Contracts.VacancyCategories.Queries;
+using MRA.Jobs.Application.Contracts.VacancyCategories.Responces;
 
 namespace MRA.Jobs.Web.Controllers;
-[Route("api/[controller]")]
-[ApiController]
+
 public class CategoryController : ApiControllerBase
 {
-    //private readonly ILogger<OidcConfigurationController> _logger;
 
-    //public CategoryController(ILogger<OidcConfigurationController> logger)
-    //{
-    //    _logger = logger;
-    //}
-    //[HttpGet]
-    //public async Task<IActionResult> Get()
-    //{
-    //    var categories =await Mediator.Send(new GetVacancyCategoryQuery());
-    //    return Ok(categories);
-    //}
-    //[HttpGet("{id}")]
-    //public IActionResult Get(int id)
-    //{
-    //    var category = Mediator.Send(new GetVacancyCategoryByIdQuery { Id = id });
-    //    return Ok(category);
-    //}
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<VacancyCategoryListDTO>>> GetAllVacancyCategories()
+    {
+        var query = new GetAllVacancyCategoryQuery();
+        var result = await Mediator.Send(query);
+        return Ok(result);
+    }
+
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<VacancyCategoryListDTO>> GetVacancyCategory(Guid id)
+    {
+        var query = new GetVacancyCategoryByIdQuery { Id = id };
+        var result = await Mediator.Send(query);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Guid>> CreateVacancyCategory(CreateVacancyCategoryCommand command)
+    {
+        var result = await Mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Guid>> UpdateVacancyCategory(Guid id, UpdateVacancyCategoryCommand command)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest();
+        }
+
+        var result = await Mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<bool>> DeleteVacancyCategory(Guid id)
+    {
+        var command = new DeleteVacancyCategoryCommand { Id = id };
+        var result = await Mediator.Send(command);
+        return Ok(result);
+    }
 }
