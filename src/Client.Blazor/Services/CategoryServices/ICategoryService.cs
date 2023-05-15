357a285deb2c81330e2db72684444f599fd596cf
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-05-15 15:27:45 +0300

    removed old client

--- src/Client.Blazor/Services/CategoryServices/ICategoryService.cs
+++
@@ -1,8 +0,0 @@
-﻿using MRA.Jobs.Application.Contracts.Applications.Queries;
-
-namespace MRA.Jobs.Client.Services.CategoryServices;
-
-public interface ICategoryService
-{
-    Task<List<GetApplicationsQuery>> GetAllCategory();
-}
