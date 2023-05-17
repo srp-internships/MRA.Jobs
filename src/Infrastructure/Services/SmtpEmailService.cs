5af366ef3455953f9bd8eb96518f6f7dba9ed83d
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-05-17 15:28:03 +0500

    Implement Error handling & logging to SmtpEmailService

--- src/Infrastructure/Services/SmtpEmailService.cs
+++ src/Infrastructure/Services/SmtpEmailService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Mail;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MRA.Jobs.Application.Common.Models;
 
@@ -8,8 +9,9 @@ public class SmtpEmailService : IEmailService
 {
     private readonly SmtpSettings _smtpSettings;
     private readonly SmtpClient _smtpClient;
+    private readonly ILogger<SmtpEmailService> _logger;
 
-    public SmtpEmailService(IOptions<SmtpSettings> smtpSettings)
+    public SmtpEmailService(IOptions<SmtpSettings> smtpSettings, ILogger<SmtpEmailService> logger)
     {
         _smtpSettings = smtpSettings.Value;
 
@@ -18,16 +20,37 @@ public class SmtpEmailService : IEmailService
             Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password),
             EnableSsl = _smtpSettings.EnableSsl
         };
+        _logger = logger;
     }
 
     public async Task SendEmailAsync(EmailMessage message)
     {
-        var mailMessage = new MailMessage(message.From, message.To, message.Subject, message.Body)
+        try
         {
-            IsBodyHtml = message.IsHtml
-        };
+            var mailMessage = new MailMessage(message.From, message.To, message.Subject, message.Body)
+            {
+                IsBodyHtml = message.IsHtml
+            };
 
-        await _smtpClient.SendMailAsync(mailMessage);
+            await _smtpClient.SendMailAsync(mailMessage);
+        }
+        catch (ArgumentNullException ex)
+        {
+            _logger.LogError($"ArgumentNull Exception: {ex.Message}");
+        }
+        catch (SmtpException ex)
+        {
+            _logger.LogError($"SMTP Exception: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Exception: {ex.Message}");
+        }
+        finally
+        {
+            _smtpClient.Dispose();
+        }
+
     }
 
 }
