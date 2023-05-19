c211ee3c962623525d7c53aa650b206d7d0646eb
Suhrob <Suhrob@DESKTOP-OPFS715>
2023-05-19 11:39:41 +0300

    category pages

---
+++ src/MRA.Jobs.Client/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using MRA.Jobs.Application.Contracts.Common;
+using MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
+using MRA.Jobs.Application.Contracts.VacancyCategories.Queries;
+using MRA.Jobs.Application.Contracts.VacancyCategories.Responces;
+
+namespace MRA.Jobs.Client.Services.CategoryServices;
+
+public interface ICategoryService
+{
+    List<VacancyCategoryListDTO> Category { get; set; }
+    UpdateVacancyCategoryCommand updatingEntity { get; set; }
+    DeleteVacancyCategoryCommand deletingEntity { get; set; }
+    CreateVacancyCategoryCommand creatingEntity { get; set; }
+    Task<List<VacancyCategoryListDTO>> GetAllCategory();
+    Task OnSaveUpdateClick();
+    Task OnDeleteClick(Guid id);
+    Task OnSaveCreateClick();
+    void OnUpdateClick(VacancyCategoryListDTO updateEntity);
+}
