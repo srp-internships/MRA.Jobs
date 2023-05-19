43595c04b672933336b94597fd89f2a3cfe2d420
Mahmud Sharipov <sharipov.m.97.97@gmail.com>
2023-05-19 10:41:33 +0500

    changed the Seive config

---
+++ src/Application/Features/JobVacancies/SieveConfigurationForJobVacancy.cs
@@ -0,0 +1,24 @@
+﻿using Sieve.Services;
+
+namespace MRA.Jobs.Application.Features.JobVacancies;
+public class SieveConfigurationForJobVacancy : ISieveConfiguration
+{
+    public void Configure(SievePropertyMapper mapper)
+    {
+        mapper.Property<JobVacancy>(p => p.Title)
+            .CanFilter()
+
+            .CanSort();
+
+        mapper.Property<JobVacancy>(p => p.Tags)
+           .CanFilter();
+
+        //mapper.Property<JobVacancy>(p => p.)
+        //    .CanSort();
+
+        //mapper.Property<JobVacancy>(p => p.DateCreated)
+        //    .CanSort()
+        //    .CanFilter()
+        //    .HasName("created_on");
+    }
+}
---
+++ src/Application/Features/VacancyCategories/SieveConfigurationForCategory.cs
@@ -0,0 +1,21 @@
+﻿using Sieve.Services;
+
+namespace MRA.Jobs.Application.Features.JobVacancies;
+public class SieveConfigurationForCategory : ISieveConfiguration
+{
+    public void Configure(SievePropertyMapper mapper)
+    {
+        mapper.Property<VacancyCategory>(p => p.Name)
+            .CanFilter()
+            .CanSort();
+
+
+        //mapper.Property<JobVacancy>(p => p.)
+        //    .CanSort();
+
+        //mapper.Property<JobVacancy>(p => p.DateCreated)
+        //    .CanSort()
+        //    .CanFilter()
+        //    .HasName("created_on");
+    }
+}
