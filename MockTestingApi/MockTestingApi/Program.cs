f630d67409fb70d4fb8b0856c32ea0eacff84f67
Islom Makhsudov <81351589+IslomMakhsudov@users.noreply.github.com>
2023-05-24 13:37:45 +0500

    Update Program.cs

--- MockTestingApi/MockTestingApi/Program.cs
+++ MockTestingApi/MockTestingApi/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
 builder.Services.AddScoped<ITestService, TestService>();
 
 var app = builder.Build();
