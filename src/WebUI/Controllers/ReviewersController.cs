70469d3273c2b87c19959515698e1dd12b48922c
nkomiljon <komiljonnajmitdinov99@gmail.com>
2023-05-24 16:35:30 +0500

    Changed ReviewersController

--- src/WebUI/Controllers/ReviewersController.cs
+++ src/WebUI/Controllers/ReviewersController.cs
@@ -2,6 +2,7 @@
 using MRA.Jobs.Application.Contracts.Common;
 using MRA.Jobs.Application.Contracts.Reviewer.Command;
 using MRA.Jobs.Application.Contracts.Reviewer.Queries;
+using MRA.Jobs.Application.Contracts.Reviewer.Response;
 
 namespace MRA.Jobs.Web.Controllers;
 
