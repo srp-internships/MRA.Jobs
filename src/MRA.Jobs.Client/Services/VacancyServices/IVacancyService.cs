b4df7967efce1a8b94408c658031770bbc691f9d
nkomiljon <komiljonnajmitdinov99@gmail.com>
2023-06-02 14:43:31 +0500

    Created Vacancy page

--- src/MRA.Jobs.Client/Services/VacancyServices/IVacancyService.cs
+++ src/MRA.Jobs.Client/Services/VacancyServices/IVacancyService.cs
@@ -1,4 +1,6 @@
-﻿using MRA.Jobs.Application.Contracts.JobVacancies.Commands;
+﻿using MRA.Jobs.Application.Contracts.Common;
+using MRA.Jobs.Application.Contracts.JobVacancies.Commands;
+using MRA.Jobs.Application.Contracts.JobVacancies.Responses;
 using MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
 using MRA.Jobs.Application.Contracts.VacancyCategories.Responses;
 
@@ -7,7 +9,9 @@ namespace MRA.Jobs.Client.Services.VacancyServices;
 public interface IVacancyService
 {
     string guidId { get; set; }
-    List<CategoryResponse> Category { get; set; }
+    List<CategoryResponse> Categories { get; set; }
+    List<JobVacancyListDTO> Vacanceies { get; set; }
+    Task<List<JobVacancyListDTO>> GetAllVacancy();
     CreateJobVacancyCommand creatingNewJob { get; set; }
     CreateVacancyCategoryCommand creatingEntity { get; set; }
     Task<List<CategoryResponse>> GetAllCategory();
--- src/MRA.Jobs.Client/Services/VacancyServices/VacancyService.cs
+++ src/MRA.Jobs.Client/Services/VacancyServices/VacancyService.cs
@@ -1,5 +1,6 @@
 ﻿using MRA.Jobs.Application.Contracts.Common;
 using MRA.Jobs.Application.Contracts.JobVacancies.Commands;
+using MRA.Jobs.Application.Contracts.JobVacancies.Responses;
 using MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
 using MRA.Jobs.Application.Contracts.VacancyCategories.Responses;
 namespace MRA.Jobs.Client.Services.VacancyServices;
@@ -24,22 +25,34 @@ public class VacancyService : IVacancyService
             PublishDate = DateTime.Now,
         };
     }
-    public List<CategoryResponse> Category { get; set; }
+    public List<CategoryResponse> Categories { get; set; }
+    
+    public List<JobVacancyListDTO> Vacanceies { get; set; }
+
     public CreateJobVacancyCommand creatingNewJob { get; set; }
+    
+    public async Task<List<JobVacancyListDTO>> GetAllVacancy()
+    {
+        var result = await _http.GetFromJsonAsync<PaggedList<JobVacancyListDTO>>("jobs");
+        Vacanceies = result.Items;
+        Console.WriteLine(Vacanceies.Count);
+        return Vacanceies;
+    }
+
     public CreateVacancyCategoryCommand creatingEntity { get; set; }
     public string guidId { get; set; } = string.Empty;
 
     public async Task<List<CategoryResponse>> GetAllCategory()
     {
         var result = await _http.GetFromJsonAsync<PaggedList<CategoryResponse>>("categories");
-        Category = result.Items;
-        return Category;
+        Categories = result.Items;
+        return Categories;
     }
+    
+    
     public async Task OnSaveCreateClick()
     {
         Console.WriteLine(creatingNewJob.CategoryId);
         await _http.PostAsJsonAsync("jobs", creatingNewJob);
-        Console.WriteLine(creatingNewJob.Title);
-        Console.WriteLine("shud");
     }
 }
