fb1632caa8a8aa58872b2036381849f9aaddff1c
endysonendy <endysonendy@gmail.com>
2023-08-11 15:39:05 +0500

    replaced magic string with constant

--- src/WebUI/AzureKeyVault/PrefixKeyVaultSecretManager.cs
+++ src/WebUI/AzureKeyVault/PrefixKeyVaultSecretManager.cs
@@ -5,9 +5,10 @@ namespace MRA.Jobs.Web.AzureKeyVault;
 
 public class PrefixKeyVaultSecretManager : KeyVaultSecretManager
 {
+    const string jobsProjectName = "MRAJobs";
     public override string GetKey(KeyVaultSecret secret)
     {
-        if (secret.Name.StartsWith("MRAJobs"))
+        if (secret.Name.StartsWith(jobsProjectName))
             return secret.Name.Replace("MRAJobs-", "").Replace("-", ConfigurationPath.KeyDelimiter);
         
         return secret.Name.Replace("-", ConfigurationPath.KeyDelimiter);
