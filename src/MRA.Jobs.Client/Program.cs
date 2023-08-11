e475a1efac4840f6dedaf3643762be2e7c6fb8f5
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-08-11 13:41:41 +0500

    HomePage & MenuButtons

--- src/MRA.Jobs.Client/Program.cs
+++ src/MRA.Jobs.Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿global using System.Net.Http.Json;
 global using MRA.Jobs.Client.Services.CategoryServices;
 global using MRA.Jobs.Client.Services.VacancyServices;
+global using Microsoft.AspNetCore.Components.Authorization;
+
 using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,6 +10,7 @@ using MRA.Jobs.Client;
 using MRA.Jobs.Client.Services.ApplicationService;
 using MRA.Jobs.Client.Services.InternshipsServices;
 using MRA.Jobs.Client.Services.TrainingServices;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -21,6 +24,9 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IVacancyService, VacancyService>();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddScoped<IInternshipService, InternshipService>();
 builder.Services.AddScoped<ITrainingService, TrainingService>();
 builder.Services.AddScoped<IApplicationService, ApplicationService>();
