084016b0bea73e72e83a571dff6236d8d142d6bb
firuz2006 <firuzisoboev@gmail.com>
2023-08-17 11:26:31 +0500

    authorization policies created & added

---
+++ src/Infrastructure/Identity/ApplicationClaimValues.cs
@@ -0,0 +1,9 @@
+﻿namespace MRA.Jobs.Infrastructure.Identity;
+
+public class ApplicationClaimValues
+{
+    public const string Administrator = "Administrator";
+    public const string Reviewer = "Reviewer";
+    public const string Applicant = "Applicant";
+    public const string ApplicationName = "Mra.Jobs";
+}
