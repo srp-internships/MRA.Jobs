f1db6ed83324191c233da5b2348147cd9651489c
endysonendy <endysonendy@gmail.com>
2023-08-18 12:16:00 +0500

    JobDetails page added and VacancyService harmlessly optimized, offering modifications described in the comments of the files.

--- src/MRA.Jobs.Client/Services/VacancyServices/IVacancyService.cs
+++ src/MRA.Jobs.Client/Services/VacancyServices/IVacancyService.cs
@@ -10,13 +10,21 @@ public interface IVacancyService
 {
     event Action OnChange;
     string guidId { get; set; }
+    public int PagesCount { get; set; }
     List<CategoryResponse> Categories { get; set; }
     List<JobVacancyListDto> Vacanceies { get; set; }
+    /* Renamed version of the too generic upper property name */
+    List<JobVacancyListDto> VacanciesOnPage { get; set; }
     //  CreateVacancyCategoryCommand creatingEntity { get; set; }
+    public int FilteredVacanciesCount { get; set; }
+    public List<JobVacancyListDto> AllVacancies { get; set; }
     CreateJobVacancyCommand creatingNewJob { get; set; }
+    public int VacanciesCountPerCategory(string categoryName);
     Task<List<JobVacancyListDto>> GetAllVacancy();
+    /* Renamed version of the upper method name with a typo */
+    Task<List<JobVacancyListDto>> GetAllVacancies();
+    Task<List<JobVacancyListDto>> GetFilteredVacancies(string title = "", string categoryName = "All categories", int page = 1);
     Task<List<CategoryResponse>> GetAllCategory();
-
     Task<List<JobVacancyListDto>> GetVacancyByTitle(string title);
     Task OnSaveCreateClick();
     Task OnDelete(string slug);
