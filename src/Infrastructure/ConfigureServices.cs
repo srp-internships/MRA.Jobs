d4961781757b6b06585300b438b9ae377224dbcb
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-08-22 16:29:28 +0500

    changes

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -28,7 +28,6 @@ public static class ConfigureServices
 
         services.AddDbContext<ApplicationDbContext>(options =>
         {
-            var env = configuration["UseInMemoryDatabase"];
             bool useInMemoryDatabase = configuration["UseInMemoryDatabase"] == "true";
             if (useInMemoryDatabase)
             {
