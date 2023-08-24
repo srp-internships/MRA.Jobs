e354606b49dbc361e4a565cddec66fc0c2f2ff23
firuz2006 <firuzisoboev@gmail.com>
2023-08-24 17:14:29 +0500

    init

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/Claim/Queries/GetBySlugQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MRA.Identity.Application.Contract.Claim.Responses;
+
+namespace MRA.Identity.Application.Contract.Claim.Queries;
+
+public class GetBySlugQuery:IRequest<ApplicationResponse<UserClaimsResponse>>
+{
+    public string Slug { get; set; } = "";
+}
