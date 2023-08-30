8fe74c3914a28e0d3fc389b47cafbf71dea1f2a6
LayloMirdadoeva <120080211+LayloMirdadoeva@users.noreply.github.com>
2023-08-29 23:52:27 -0700

    UserRolesHandler

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/UserRoles/Commands/CreateUserRolesCommand.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/UserRoles/Commands/CreateUserRolesCommand.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 using MediatR;
 
 namespace MRA.Identity.Application.Contract.UserRoles.Commands;
-public class CreateUserRolesCommand : IRequest<ApplicationResponse<Guid>>
+public class CreateUserRolesCommand : IRequest<ApplicationResponse<string>>
 {
     public Guid RoleId { get; set; }
     public Guid UserId { get; set; }
+
 }
--- src/MRA.Jobs.Identity/MRA.Identity.Application/Features/UserRoles/Commands/CreateUserRoleCommandHandler.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/UserRoles/Commands/CreateUserRoleCommandHandler.cs
@@ -7,50 +7,59 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MRA.Identity.Application.Common.Interfaces.DbContexts;
 using MRA.Identity.Application.Contract;
 using MRA.Identity.Application.Contract.UserRoles.Commands;
 using MRA.Identity.Domain.Entities;
 
 namespace MRA.Identity.Application.Features.UserRoles.Commands;
-public class CreateUserRoleCommandHandler : IRequestHandler<CreateUserRolesCommand, ApplicationResponse<Guid>>
+public class CreateUserRoleCommandHandler : IRequestHandler<CreateUserRolesCommand, ApplicationResponse<string>>
 {
     private readonly RoleManager<ApplicationRole> _roleManager;
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IApplicationDbContext _applicationDbContext;
 
     public CreateUserRoleCommandHandler(RoleManager<ApplicationRole> roleManager,
-        UserManager<ApplicationUser> userManager)
+        UserManager<ApplicationUser> userManager, IApplicationDbContext applicationDbContext)
     {
         _roleManager = roleManager;
         _userManager = userManager;
+        _applicationDbContext = applicationDbContext;
     }
-    public async Task<ApplicationResponse<Guid>> Handle(CreateUserRolesCommand request, CancellationToken cancellationToken)
+    public async Task<ApplicationResponse<string>> Handle(CreateUserRolesCommand request, CancellationToken cancellationToken)
     {
         try
         {
             var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == request.UserId);
             if (user == null)
             {
-                return new ApplicationResponseBuilder<Guid>().SetErrorMessage("User not found!").Success(false).Build();
+                return new ApplicationResponseBuilder<string>().SetErrorMessage("User not found!").Success(false).Build();
             }
 
             var role = await _roleManager.Roles.FirstOrDefaultAsync(r => r.Id == request.RoleId);
             if (role == null)
             {
-                return new ApplicationResponseBuilder<Guid>().SetErrorMessage("Role not found!").Success(false).Build();
+                return new ApplicationResponseBuilder<string>().SetErrorMessage("Role not found!").Success(false).Build();
             }
 
             var result = await _userManager.AddToRoleAsync(user, role.Name);
-
-            if (result.Succeeded)
+            var newUserRole = new ApplicationUserRoles
             {
-                return new ApplicationResponseBuilder<Guid>().SetResponse(user.Id).Build();
-            }
-            return new ApplicationResponseBuilder<Guid>().SetErrorMessage(result.Errors.First().Description).Success(false).Build();
+                UserId = request.UserId,
+                RoleId = request.RoleId,
+                Slug = $"{user.UserName}-{role.Slug}"
+            };
+            await _applicationDbContext.UserRoles.AddAsync(newUserRole);
+            await _applicationDbContext.SaveChangesAsync(); 
+            
+                return new ApplicationResponseBuilder<string>().SetResponse(newUserRole.Slug).Build();
+            
+           // return new ApplicationResponseBuilder<Guid>().SetErrorMessage(result.Errors.First().Description).Success(false).Build();
 
         }
         catch (Exception e)
         {
-            return new ApplicationResponseBuilder<Guid>().SetException(e).Success(false).Build();
+            return new ApplicationResponseBuilder<string>().SetException(e).Success(false).Build();
         }
 
     }
