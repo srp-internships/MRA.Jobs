e3f9b2d49c933f77b86244bd0aa9e57ec6cf4629
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-09-01 16:03:23 +0500

    VacancyCategoryCommandsTests

--- tests/MRA.Jobs.Api/Application.IntegrationTests/VacancyCategories/Commands/CreateVacancyCategoryCommandTest.cs
+++ tests/MRA.Jobs.Api/Application.IntegrationTests/VacancyCategories/Commands/CreateVacancyCategoryCommandTest.cs
@@ -1,9 +1,10 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
+using System.Net.Http.Json;
 using FluentAssertions;
 using MRA.Jobs.Application.Contracts.VacancyCategories.Commands;
 using NUnit.Framework;
 namespace MRA.Jobs.Application.IntegrationTests.VacancyCategories.Commands;
-public class CreateVacancyCategoryCommandTest:Testing
+public class CreateVacancyCategoryCommandTest : Testing
 {
     [Test]
     public async Task CreateVacancyCategoryCommand_ShouldCreateVacancyCategory_Success()
@@ -12,10 +13,7 @@ public class CreateVacancyCategoryCommandTest:Testing
 
         var response = await _httpClient.PostAsJsonAsync("/api/categories", category);
 
-        response.EnsureSuccessStatusCode();
-
-        var responseGuid = await response.Content.ReadAsStringAsync();
-
-        responseGuid.Should().NotBeEmpty();
+        Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            
     }
 }
