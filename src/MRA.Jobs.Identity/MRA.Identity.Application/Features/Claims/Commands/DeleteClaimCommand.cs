378485c4fc3ecb875248ee314e8ef033625fc01e
firuz2006 <firuzisoboev@gmail.com>
2023-09-04 13:41:55 +0500

    claims tests and email service

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Claims/Commands/DeleteClaimCommand.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MRA.Identity.Application.Common.Interfaces.DbContexts;
+using MRA.Identity.Application.Contract;
+using MRA.Identity.Application.Contract.Claim.Commands;
+
+namespace MRA.Identity.Application.Features.Claims.Commands;
+
+public class DeleteClaimCommandHandler : IRequestHandler<DeleteClaimCommand, ApplicationResponse>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteClaimCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ApplicationResponse> Handle(DeleteClaimCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var claim = await _context.UserClaims.SingleOrDefaultAsync(s => s.Slug == request.Slug.Trim().ToLower(), cancellationToken: cancellationToken);
+            if (claim==null)
+            {
+                return new ApplicationResponseBuilder().Success(false)
+                    .SetErrorMessage($"claim with slug {request.Slug} not found").Build();
+            }
+
+            _context.UserClaims.Remove(claim);
+            
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return new ApplicationResponseBuilder().Build();
+        }
+        catch (Exception e)
+        {
+            return new ApplicationResponseBuilder().Success(false).SetException(e).Build();
+        }
+    }
+}
---
+++ tests/MRA.Jobs.Identity/Application.IntegrationTests/ClaimsTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using MRA.Identity.Application.Contract.Claim.Commands;
+using MRA.Identity.Domain.Entities;
+
+namespace MRA.Jobs.Application.IntegrationTests;
+
+public class ClaimsTests : BaseTest
+{
+    private async Task<Guid> CreateSampleUserAsync()
+    {
+        var user = new ApplicationUser
+        {
+            Id = Guid.NewGuid(),
+            UserName = "username",
+            NormalizedUserName = "USERNAME",
+            Email = "email@gmail.com",
+            NormalizedEmail = "EMAIL@GMAIL.COM",
+            EmailConfirmed = false,
+            PhoneNumber = "1234567",
+            PhoneNumberConfirmed = false,
+        };
+        await AddUser(user, "A2dAsdf@");
+        return user.Id;
+    }
+
+    //create claim
+    [Test]
+    public async Task CreateClaim_ValidRequest_ReturnsOkResult()
+    {
+        var userId = await CreateSampleUserAsync();
+
+        var createCommand = new CreateClaimCommand
+        {
+            UserId = userId, ClaimType = "Application", ClaimValue = "Mra.Identity"
+        };
+
+        HttpResponseMessage createResponse = await _client.PostAsJsonAsync("api/claims", createCommand);
+
+        Assert.That(createResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task CreateClaim_InvalidUserIdRequest_ReturnsUserNotFound()
+    {
+        await CreateSampleUserAsync();
+
+        var createCommand = new CreateClaimCommand
+        {
+            UserId = Guid.Empty, ClaimType = "Application", ClaimValue = "Mra.Identity"
+        };
+
+        HttpResponseMessage createResponse = await _client.PostAsJsonAsync("api/claims", createCommand);
+
+        Assert.That(createResponse.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        Assert.That(await createResponse.Content.ReadAsStringAsync(), Does.Contain("user is not found"));
+    }
+    //create claim
+
+    //put claim
+    [Test]
+    public async Task Update_ValidRequest_ReturnsOkResult()
+    {
+        var userId = await CreateSampleUserAsync();
+
+        var sampleClaim = new ApplicationUserClaim
+        {
+            Id = 0,
+            UserId = userId,
+            ClaimType = "Application",
+            ClaimValue = "Mra.Test",
+            Slug = "user1-application"
+        };
+
+        await AddEntity(sampleClaim);
+
+        var putCommand = new UpdateClaimCommand { ClaimValue = "Mra.Test", Slug = "user1-application" };
+        HttpResponseMessage response = await _client.PutAsJsonAsync($"api/claims/{sampleClaim.Slug}", putCommand);
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        ApplicationUserClaim updatedClaim = await GetEntity<ApplicationUserClaim>(s => s.Id == sampleClaim.Id);
+        updatedClaim.ClaimValue.Should().Be(sampleClaim.ClaimValue);
+    }
+
+    //put claim
+
+
+    //delete claim
+    [Test]
+    public async Task Delete_ValidRequest_ReturnsOkResult()
+    {
+        var userId = await CreateSampleUserAsync();
+
+        var sampleClaim = new ApplicationUserClaim
+        {
+            Id = 0,
+            UserId = userId,
+            ClaimType = "Application",
+            ClaimValue = "Mra.Test",
+            Slug = "user1-application"
+        };
+
+        await AddEntity(sampleClaim);
+
+        HttpResponseMessage response = await _client.DeleteAsync($"api/Claims/{sampleClaim.Slug}");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        (await CountAsync<ApplicationUserClaim>()).Should().Be(0);
+    }
+
+    //delete claim
+}
