7cd83e45ec08743ace9683f072e8740a15f240bd
firuz2006 <firuzisoboev@gmail.com>
2023-09-05 20:40:00 +0500

    fix

--- src/MRA.Jobs.Identity/MRA.Identity.Api/Controllers/ClaimsController.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Api/Controllers/ClaimsController.cs
@@ -11,7 +11,6 @@ namespace MRA.Identity.Api.Controllers;
 
 [ApiController]
 [Route("/api/[controller]/")]
-[Authorize(ApplicationPolicies.Administrator)]
 public class ClaimsController : ControllerBase
 {
     private readonly ISender _mediator;
