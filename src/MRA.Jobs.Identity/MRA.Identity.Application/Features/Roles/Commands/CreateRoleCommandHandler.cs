25d72e9139a3b707add96f42cf35552b23b349ee
firuz2006 <firuzisoboev@gmail.com>
2023-09-07 09:57:08 +0500

    fix trash-code

--- src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Roles/Commands/CreateRoleCommandHandler.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Roles/Commands/CreateRoleCommandHandler.cs
@@ -5,38 +5,40 @@ using MRA.Identity.Application.Contract.ApplicationRoles.Commands;
 using MRA.Identity.Domain.Entities;
 
 namespace MRA.Identity.Application.Features.Roles.Commands;
+
 public class CreateRoleCommandHandler : IRequestHandler<CreateRoleCommand, ApplicationResponse<Guid>>
 {
     private readonly RoleManager<ApplicationRole> _roleManager;
+
     public CreateRoleCommandHandler(RoleManager<ApplicationRole> roleManager)
     {
         _roleManager = roleManager;
     }
+
     public async Task<ApplicationResponse<Guid>> Handle(CreateRoleCommand request, CancellationToken cancellationToken)
     {
         try
         {
-            var role = new ApplicationRole()
+            var role = new ApplicationRole
             {
                 Id = Guid.NewGuid(),
                 Name = request.RoleName,
                 NormalizedName = request.RoleName.ToLower(),
                 Slug = request.RoleName.ToLower(),
             };
-            var test = _roleManager.Roles.ToList();
+            
             var result = await _roleManager.CreateAsync(role);
-            test = _roleManager.Roles.ToList();
             if (result.Succeeded)
             {
                 return new ApplicationResponseBuilder<Guid>().SetResponse(role.Id).Build();
             }
 
-            return new ApplicationResponseBuilder<Guid>().SetErrorMessage(result.Errors.First().Description).Success(false).Build();
+            return new ApplicationResponseBuilder<Guid>().SetErrorMessage(result.Errors.First().Description)
+                .Success(false).Build();
         }
         catch (Exception e)
         {
             return new ApplicationResponseBuilder<Guid>().Success(false).SetException(e).Build();
         }
     }
-
 }
