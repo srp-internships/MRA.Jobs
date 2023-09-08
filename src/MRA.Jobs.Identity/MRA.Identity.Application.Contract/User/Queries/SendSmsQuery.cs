cc151b9de403dea05c957122e8a6ae97fe3970ef
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-09-08 21:44:55 +0500

    changes

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Queries/SendSmsQuery.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Queries/SendSmsQuery.cs
@@ -1,7 +1,7 @@
 ﻿using MediatR;
 
 namespace MRA.Identity.Application.Contract.User.Queries;
-public class SendSmsQuery : IRequest<string>
+public class SendSmsQuery : IRequest<bool>
 {
     public string PhoneNumber { get; set; }
 }
--- src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Users/Query/SendSmsQueryHandler.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Users/Query/SendSmsQueryHandler.cs
@@ -1,19 +1,30 @@
 ﻿using MediatR;
-using MRA.Identity.Application.Common.Interfaces.Services;
+using Mra.Shared.Common.Interfaces.Services;
+using MRA.Identity.Application.Common.Interfaces.DbContexts;
 using MRA.Identity.Application.Contract.User.Queries;
 
+
 namespace MRA.Identity.Application.Features.Users.Query;
-public class SendSmsQueryHandler : IRequestHandler<SendSmsQuery, string>
+public class SendSmsQueryHandler : IRequestHandler<SendSmsQuery, bool>
 {
+    private readonly IApplicationDbContext _context;
     private readonly ISmsService _smsService;
 
-    public SendSmsQueryHandler(ISmsService smsService)
+    public SendSmsQueryHandler(IApplicationDbContext context, ISmsService smsService)
     {
+        _context = context;
         _smsService = smsService;
     }
-    public async Task<string> Handle(SendSmsQuery request, CancellationToken cancellationToken)
+    public async Task<bool> Handle(SendSmsQuery request, CancellationToken cancellationToken)
     {
-        var response = await _smsService.SendSms(request.PhoneNumber);
-        return response;
+        var code = await _smsService.SendSmsAsync(request.PhoneNumber);
+        _context.ConfirmationCodes.Add(new Domain.Entities.ConfirmationCode
+        {
+            Code = code,
+            PhoneNumber = request.PhoneNumber,
+        });
+        await _context.SaveChangesAsync();
+
+        return true;
     }
 }
