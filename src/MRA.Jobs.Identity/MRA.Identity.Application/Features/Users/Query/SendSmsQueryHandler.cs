6b7e914be0e6f35c14dea3284a58efc285fe7c09
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-09-13 10:09:42 +0500

    commit

--- src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Users/Query/SendSmsQueryHandler.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Users/Query/SendSmsQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Mra.Shared.Common.Interfaces.Services;
 using MRA.Identity.Application.Common.Interfaces.DbContexts;
 using MRA.Identity.Application.Contract.User.Queries;
@@ -9,25 +10,42 @@ public class SendSmsQueryHandler : IRequestHandler<SendSmsQuery, bool>
 {
     private readonly IApplicationDbContext _context;
     private readonly ISmsService _smsService;
+    private readonly ILogger _logger;
 
-    public SendSmsQueryHandler(IApplicationDbContext context,  ISmsService smsService)
+    public SendSmsQueryHandler(IApplicationDbContext context,  ISmsService smsService, ILogger logger)
     {
         _context = context;
         _smsService = smsService;
+        _logger = logger;
     }
     public async Task<bool> Handle(SendSmsQuery request, CancellationToken cancellationToken)
     {
-        var code = await _smsService.SendSmsAsync(request.PhoneNumber);
+        int code;
+        var response = await _smsService.SendSmsAsync(request.PhoneNumber, GenerateMessage(out code));
 
-        if (code == -1) return false;
+        if (!response) return false;
 
-        _context.ConfirmationCodes.Add(new Domain.Entities.ConfirmationCode
+        try
         {
-            Code = code,
-            PhoneNumber = request.PhoneNumber,
-        });
-        await _context.SaveChangesAsync();
+            _context.ConfirmationCodes.Add(new Domain.Entities.ConfirmationCode
+            {
+                Code = code,
+                PhoneNumber = request.PhoneNumber,
+            });
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            return false;
+        }
+    }
 
-        return true;
+    private static string GenerateMessage(out int code)
+    {
+        Random random = new Random();
+        code = random.Next(1000, 10001);
+        return $"Your confirmation code is: {code}. Please enter this code to verify your phone number.";
     }
 }
