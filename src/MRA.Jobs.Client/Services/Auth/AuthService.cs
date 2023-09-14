c8c98cdbb45bad059150fbc6b04d0ccbdff408e6
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-09-14 17:15:03 +0500

    login page

--- src/MRA.Jobs.Client/Services/Auth/AuthService.cs
+++ src/MRA.Jobs.Client/Services/Auth/AuthService.cs
@@ -1,8 +1,10 @@
-﻿
-using System.Net;
+﻿using System.Net;
+using MRA.Identity.Application.Contract;
+using MRA.Identity.Application.Contract.Admin.Responses;
 using MRA.Identity.Application.Contract.User.Commands;
 
 namespace MRA.Jobs.Client.Services.Auth;
+
 
 public class AuthService : IAuthService
 {
@@ -12,11 +14,30 @@ public class AuthService : IAuthService
     {
         _httpClient = httpClient;
     }
+
+    public async Task<JwtTokenResponse> LoginUserAsync(LoginUserCommand command)
+    {
+        var result = await _httpClient.PostAsJsonAsync($"{IdentityURL.Auth}login", command);
+        if (result.IsSuccessStatusCode)
+        {
+            var response = await result.Content.ReadFromJsonAsync<JwtTokenResponse>();
+            return response;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
     public async Task<bool> RegisterUserAsync(RegisterUserCommand command)
     {
         var result = await _httpClient.PostAsJsonAsync($"{IdentityURL.Auth}register", command);
-        if(result.StatusCode==HttpStatusCode.OK) 
+        if (result.StatusCode == HttpStatusCode.OK)
             return true;
         return false;
     }
 }
+
+
+
--- src/MRA.Jobs.Client/Services/Auth/IAuthService.cs
+++ src/MRA.Jobs.Client/Services/Auth/IAuthService.cs
@@ -1,8 +1,11 @@
-﻿using MRA.Identity.Application.Contract.User.Commands;
+﻿using MRA.Identity.Application.Contract;
+using MRA.Identity.Application.Contract.Admin.Responses;
+using MRA.Identity.Application.Contract.User.Commands;
 
 namespace MRA.Jobs.Client.Services.Auth;
 
 public interface IAuthService
 {
     Task<bool> RegisterUserAsync(RegisterUserCommand command);
+    Task<JwtTokenResponse> LoginUserAsync(LoginUserCommand command);
 }
