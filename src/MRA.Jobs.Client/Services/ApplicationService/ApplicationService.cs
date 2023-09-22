4db2e283b70dc9883994d23cce5cc6e296687217
Azimjon Fayzulloev <sherman.peabody99@gmail.com>
2023-09-22 14:17:08 +0500

    commit

--- src/MRA.Jobs.Client/Services/ApplicationService/ApplicationService.cs
+++ src/MRA.Jobs.Client/Services/ApplicationService/ApplicationService.cs
@@ -1,4 +1,5 @@
-﻿using MRA.Jobs.Application.Contracts.Applications.Commands.CreateApplication;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using MRA.Jobs.Application.Contracts.Applications.Commands.CreateApplication;
 using MRA.Jobs.Application.Contracts.Applications.Responses;
 using static MRA.Jobs.Application.Contracts.Dtos.Enums.ApplicationStatusDto;
 
@@ -8,10 +9,12 @@ namespace MRA.Jobs.Client.Services.ApplicationService;
 public class ApplicationService : IApplicationService
 {
     private readonly HttpClient _httpClient;
+    private readonly AuthenticationStateProvider _authenticationState;
 
-    public ApplicationService(HttpClient httpClient)
+    public ApplicationService(HttpClient httpClient, AuthenticationStateProvider authenticationState)
     {
         _httpClient = httpClient;
+        _authenticationState = authenticationState;
     }
 
     public async Task<List<ApplicationListStatus>> GetApplicationsByStatus(ApplicationStatus status)
@@ -30,7 +33,8 @@ public class ApplicationService : IApplicationService
 
     public async Task<List<ApplicationListDto>> GetAllApplications()
     {
-
+        await _authenticationState.GetAuthenticationStateAsync();
+        var header = _httpClient.DefaultRequestHeaders.Authorization;
         HttpResponseMessage response = await _httpClient.GetAsync("/api/applications/");
         List<ApplicationListDto> result = await response.Content.ReadFromJsonAsync<List<ApplicationListDto>>();
         return result;
