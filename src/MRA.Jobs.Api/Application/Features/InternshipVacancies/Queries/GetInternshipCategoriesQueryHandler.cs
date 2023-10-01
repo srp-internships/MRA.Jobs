d8c428c3156028dd3e5b13390802a6500b923883
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-01 20:24:47 +0500

    creatind GetInternshipCategoriesQueryHandler

---
+++ src/MRA.Jobs.Api/Application/Features/InternshipVacancies/Queries/GetInternshipCategoriesQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using MRA.Jobs.Application.Contracts.InternshipVacancies.Queries;
+using MRA.Jobs.Application.Contracts.InternshipVacancies.Responses;
+
+namespace MRA.Jobs.Application.Features.InternshipVacancies.Queries;
+public class GetInternshipCategoriesQueryHandler : IRequestHandler<GetInternshipCategoriesQuery, List<InternshipCategoriesResponce>>
+{
+
+    public Task<List<InternshipCategoriesResponce>> Handle(GetInternshipCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
