28054418530c6d19678a37d5a801b787930623d5
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-10-04 10:15:19 +0500

    Profile controller

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Api/Controllers/ProfileController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MRA.Identity.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProfileController : ControllerBase
+{
+}
