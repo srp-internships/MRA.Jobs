eb5799c9822e1775222a84a327582c7d99727949
adheeeem <sherman.peabody99@gmail.com>
2023-10-05 11:40:32 +0500

    commit

--- src/MRA.Jobs.Api/Infrastructure/Persistence/DbMigration.cs
+++ src/MRA.Jobs.Api/Infrastructure/Persistence/DbMigration.cs
@@ -5,6 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MRA.Jobs.Infrastructure.Persistence;
-internal class DbMigration
+public class DbMigration : IMigration
 {
+    private readonly ApplicationDbContext context;
+
+    public DbMigration(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+
+    public void Migrate()
+    {
+        if (context.Database.IsRelational())
+        {
+            context.Database.Migrate();
+        }
+    }
+}
+
+public interface IMigration
+{
+    void Migrate();
 }
