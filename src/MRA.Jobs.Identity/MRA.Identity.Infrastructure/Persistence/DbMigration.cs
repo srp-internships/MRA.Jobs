33bc43ac182e4c68b9bc9a10129753aa2cb13b64
adheeeem <sherman.peabody99@gmail.com>
2023-10-05 11:46:00 +0500

    migration for identity service

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Infrastructure/Persistence/DbMigration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.Identity.Infrastructure.Persistence;
+internal class DbMigration
+{
+}
