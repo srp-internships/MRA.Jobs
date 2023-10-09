9d0c4fcd59aef07bef9543e6e37fc51f18d21594
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-09 10:24:32 +0500

    Add ApiValidationFilterAttribute

---
+++ src/MRA.Jobs.Api/WebUI/ApiValidationFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MRA.Jobs.Application.Common.Exceptions;
+
+namespace MRA.Jobs.Application.Contracts;
+internal class ApiValidationFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        if (context.Exception is ValidationException exception)
+        {
+            context.ExceptionHandled = true;
+            context.Result = new BadRequestObjectResult(exception.Errors);
+            return;
+        }
+        else if (context.Exception is NotFoundException)
+        {
+            context.ExceptionHandled = true;
+            context.Result = new NotFoundResult();
+            return;
+        }
+        base.OnException(context);
+    }
+}
