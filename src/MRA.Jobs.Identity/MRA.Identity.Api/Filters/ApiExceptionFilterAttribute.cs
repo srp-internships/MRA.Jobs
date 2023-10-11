fafc661beefe1adc9d0c3ca0c46b2d4a83f4eb47
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-11 11:44:29 +0500

    Add filter

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Api/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MRA.Identity.Application.Common.Exceptions;
+
+namespace MRA.Jobs.Web.Filters;
+
+public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        context.ExceptionHandled = context switch
+        {
+            { Exception: ValidationException } => HandleValidationException(context),
+            { Exception: NotFoundException } => HandleNotFoundException(context),
+            { Exception: UnauthorizedAccessException } => HandleUnauthorizedAccessException(context),
+            { Exception: ForbiddenAccessException } => HandleForbiddenAccessException(context),
+            { Exception: TaskCanceledException } => HandleTaskCanceledException(context),
+            { ModelState: { IsValid: false } } => HandleInvalidModelStateException(context),
+            _ => HandleUnknownException(context)
+        };
+
+        base.OnException(context);
+    }
+
+    public bool HandleTaskCanceledException(ExceptionContext context)
+    {
+        ProblemDetails details = new ProblemDetails
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Title = "Request was canceled.",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+        };
+
+        context.Result = new ObjectResult(details) { StatusCode = StatusCodes.Status400BadRequest };
+
+        return true;
+    }
+
+    public bool HandleUnknownException(ExceptionContext context)
+    {
+        ProblemDetails details = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "An error occurred while processing your request.",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+            Detail = context.Exception.Message,
+            Extensions =
+            {
+                { "traceId", context.HttpContext.TraceIdentifier },
+                { "stackTrace", context.Exception.StackTrace }
+            }
+        };
+
+        context.Result = new ObjectResult(details) { StatusCode = StatusCodes.Status500InternalServerError };
+
+        return true;
+    }
+
+    private bool HandleValidationException(ExceptionContext context)
+    {
+        ValidationException exception = (ValidationException)context.Exception;
+
+        ValidationProblemDetails details = new ValidationProblemDetails(exception.Errors)
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+        };
+
+        context.Result = new BadRequestObjectResult(details);
+
+        return true;
+    }
+
+    private bool HandleInvalidModelStateException(ExceptionContext context)
+    {
+        ValidationProblemDetails details = new ValidationProblemDetails(context.ModelState)
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
+        };
+
+        context.Result = new BadRequestObjectResult(details);
+
+        return true;
+    }
+
+    private bool HandleNotFoundException(ExceptionContext context)
+    {
+        NotFoundException exception = (NotFoundException)context.Exception;
+
+        ProblemDetails details = new ProblemDetails
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = "The specified resource was not found.",
+            Detail = exception.Message
+        };
+
+        context.Result = new NotFoundObjectResult(details);
+
+        return true;
+    }
+
+    private bool HandleUnauthorizedAccessException(ExceptionContext context)
+    {
+        ProblemDetails details = new ProblemDetails
+        {
+            Status = StatusCodes.Status401Unauthorized,
+            Title = "Unauthorized",
+            Type = "https://tools.ietf.org/html/rfc7235#section-3.1"
+        };
+
+        context.Result = new ObjectResult(details) { StatusCode = StatusCodes.Status401Unauthorized };
+
+        return true;
+    }
+
+    private bool HandleForbiddenAccessException(ExceptionContext context)
+    {
+        ProblemDetails details = new ProblemDetails
+        {
+            Status = StatusCodes.Status403Forbidden,
+            Title = "Forbidden",
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.3"
+        };
+
+        context.Result = new ObjectResult(details) { StatusCode = StatusCodes.Status403Forbidden };
+
+        return true;
+    }
+}
---
+++ src/MRA.Jobs.Identity/MRA.Identity.Api/Filters/BadRequestResponseFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MRA.Jobs.Web;
+public class BadRequestResponseFilter : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            var errors = context.ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage);
+            var errorResponse = new BadRequestResponse
+            {
+                Errors = errors.Select(e => new Error { Title = "Details", Details = e }).ToList()
+            };
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+    }
+}
+
+public class BadRequestResponse
+{
+    public List<Error> Errors { get; set; }
+}
+
+public class Error
+{
+    public string Title { get; set; }
+    public string Details { get; set; }
+}
+
