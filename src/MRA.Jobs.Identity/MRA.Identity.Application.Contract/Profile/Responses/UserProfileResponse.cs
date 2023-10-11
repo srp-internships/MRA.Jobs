03f04df5035d076923bc8c5267343ee98d141268
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-10-11 17:18:31 +0500

    layout example profile page

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/Profile/Responses/UserProfileResponse.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/Profile/Responses/UserProfileResponse.cs
@@ -9,5 +9,7 @@ public class UserProfileResponse
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string AboutMyself { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }   
 
 }
