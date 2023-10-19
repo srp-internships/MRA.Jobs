ef20f5b5854f229f66892ce378f523c492ac1a2a
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-19 09:16:29 +0500

    delete

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/Application/Responses/ApplicationNameResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace MRA.Identity.Application.Contract.Application.Responses;
-
-public class ApplicationNameResponse
-{
-    public string Name { get; set; }
-    public DateTime DateOfCreate { get; set; }
-}
