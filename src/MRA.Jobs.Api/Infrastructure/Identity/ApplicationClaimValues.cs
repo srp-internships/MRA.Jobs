13885f55c9f1f021cf86b20b28bf8a3073a7a586
firuz2006 <firuzisoboev@gmail.com>
2023-10-27 11:22:47 +0500

    add super admin role to policy

--- src/MRA.Jobs.Api/Infrastructure/Identity/ApplicationClaimValues.cs
+++ src/MRA.Jobs.Api/Infrastructure/Identity/ApplicationClaimValues.cs
@@ -6,4 +6,5 @@ public class ApplicationClaimValues
     public const string Reviewer = "Reviewer";
     public const string Applicant = "Applicant";
     public const string ApplicationName = "MraJobs";
+    public const string SuperAdmin = "SuperAdmin";
 }
