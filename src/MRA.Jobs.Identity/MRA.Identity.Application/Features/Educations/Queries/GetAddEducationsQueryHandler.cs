75103f891a180f2751b3dc8f7a762ad76b7f13ef
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-10-27 11:30:15 +0500

    commit

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Educations/Queries/GetAddEducationsQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using MRA.Identity.Application.Contract.Educations.Query;
+using MRA.Identity.Application.Contract.Educations.Responses;
+
+namespace MRA.Identity.Application.Features.Educations.Queries;
+public class GetAddEducationsQueryHandler : IRequestHandler<GetAllEducationsQuery, List<UserEducationResponse>>
+{
+    public Task<List<UserEducationResponse>> Handle(GetAllEducationsQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
