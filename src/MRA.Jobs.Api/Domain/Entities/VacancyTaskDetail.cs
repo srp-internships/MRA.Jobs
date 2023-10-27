c7673738509bcdc37e573f34f75ece85ddf2f25d
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-27 16:10:33 +0500

    Add VacancyTaskDetail

---
+++ src/MRA.Jobs.Api/Domain/Entities/VacancyTaskDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace MRA.Jobs.Domain.Entities;
+public class VacancyTaskDetail
+{
+    public Guid Id { get; set; }
+    public Guid ApplicantId { get; set; }
+    public Guid TaskId { get; set; }
+    public bool Success { get; init; }
+    public string Codes { get; set; }
+
+}
