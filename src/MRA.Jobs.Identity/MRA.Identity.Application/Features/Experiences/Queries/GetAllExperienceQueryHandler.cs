be1afb5bda486328be65c3fcff45a4f50a01eaef
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-10-30 14:05:04 +0500

    Experience MudAutoComplate

--- src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Experiences/Queries/GetAllExperienceQueryHandler.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Experiences/Queries/GetAllExperienceQueryHandler.cs
@@ -1,12 +1,27 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MRA.Identity.Application.Common.Interfaces.DbContexts;
 using MRA.Identity.Application.Contract.Experiences.Queries;
 using MRA.Identity.Application.Contract.Experiences.Responses;
 
 namespace MRA.Identity.Application.Features.Experiences.Queries;
 public class GetAllExperienceQueryHandler : IRequestHandler<GetAllExperienceQuery, List<UserExperienceResponse>>
 {
-    public Task<List<UserExperienceResponse>> Handle(GetAllExperienceQuery request, CancellationToken cancellationToken)
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public GetAllExperienceQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+    public async Task<List<UserExperienceResponse>> Handle(GetAllExperienceQuery request, CancellationToken cancellationToken)
+    {
+        var experiences = await _dbContext.Experiences
+             .ToListAsync();
+        var response = experiences.
+            Select(e => _mapper.Map<UserExperienceResponse>(e)).ToList();
+        return response;
     }
 }
