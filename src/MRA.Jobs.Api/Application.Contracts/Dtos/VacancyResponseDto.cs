e1fb0abdbbc346f087c81a1b1102b249ec50c130
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-30 16:07:07 +0500

    update branch

--- src/MRA.Jobs.Api/Application.Contracts/Dtos/VacancyResponseDto.cs
+++ src/MRA.Jobs.Api/Application.Contracts/Dtos/VacancyResponseDto.cs
@@ -1,6 +1,4 @@
-﻿using MRA.Jobs.Application.Contracts.Dtos.Responses;
-
-namespace MRA.Jobs.Application.Contracts.Dtos;
+﻿namespace MRA.Jobs.Application.Contracts.Dtos;
 public class VacancyResponseDto
 {
     public string Response { get; set; }
