d834c00da07888260c1b23cf58d8d81187a35d0b
firuz2006 <firuzisoboev@gmail.com>
2023-10-30 13:43:55 +0500

    add NORMALLY jwt parser

--- src/MRA.Jobs.Client/CustomAuthStateProvider.cs
+++ src/MRA.Jobs.Client/CustomAuthStateProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text.Json;
@@ -79,31 +80,10 @@ public class CustomAuthStateProvider : AuthenticationStateProvider
 
         return token;
     }
-
-    private byte[] ParseBase64WithoutPadding(string base64)
-    {
-        switch (base64.Length % 4)
-        {
-            case 2:
-                base64 += "==";
-                break;
-            case 3:
-                base64 += "=";
-                break;
-        }
-
-        return Convert.FromBase64String(base64);
-    }
-
+    
     private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
     {
-        var payload = jwt.Split('.')[1];
-        var jsonBytes = ParseBase64WithoutPadding(payload);
-        var keyValuePairs = JsonSerializer
-            .Deserialize<Dictionary<string, object>>(jsonBytes);
-
-        var claims = keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
-
-        return claims;
+        var jwtObject = new JwtSecurityTokenHandler().ReadJwtToken(jwt);
+        return jwtObject.Claims;
     }
 }
