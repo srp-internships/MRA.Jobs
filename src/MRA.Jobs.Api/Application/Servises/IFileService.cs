c0422503b3802899938d061ca071e882fff7450d
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-11-07 19:42:03 +0500

    FileServise

---
+++ src/MRA.Jobs.Api/Application/Servises/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MRA.Jobs.Application.Servises;
+public interface IFileService
+{
+    Task<string> UploadAsync(IFormFile file);
+    byte[] Download(string key);
+    bool FileExists(string key);
+}
