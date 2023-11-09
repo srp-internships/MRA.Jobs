182f9090a1a53de2c8daa5fd9a10bbb0f4b11f11
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-11-09 15:15:01 +0500

    UpdateUserEducationDetailCommand_ShouldFail_WhenDuplicateExists

--- tests/MRA.Jobs.Identity/Application.IntegrationTests/Educations/Commands/UpdateUserEducationDetailCommandTest.cs
+++ tests/MRA.Jobs.Identity/Application.IntegrationTests/Educations/Commands/UpdateUserEducationDetailCommandTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc;
 using MRA.Identity.Application.Contract.Educations.Command.Update;
 using MRA.Identity.Domain.Entities;
 
@@ -35,5 +37,49 @@ public class UpdateUserEducationDetailCommandTest : BaseTest
         var response2 = await _client.PutAsJsonAsync("/api/Profile/UpdateEducationDetail", command);
 
         response2.EnsureSuccessStatusCode();
+    }
+
+    [Test]
+    public async Task UpdateUserEducationDetailCommand_ShouldFail_WhenDuplicateExists()
+    {
+        await AddApplicantAuthorizationAsync();
+
+        var education1 = new EducationDetail()
+        {
+            Speciality = "test",
+            University = "test",
+            StartDate = DateTime.Now.AddYears(-5),
+            EndDate = DateTime.Now.AddYears(-1),
+            UntilNow = false,
+            UserId = Applicant.Id,
+        };
+
+        var education2 = new EducationDetail()
+        {
+            Speciality = "test2",
+            University = "test2",
+            StartDate = DateTime.Now.AddYears(-5),
+            EndDate = DateTime.Now.AddYears(-1),
+            UntilNow = false,
+            UserId = Applicant.Id,
+        };
+
+        await AddEntity(education1);
+        await AddEntity(education2);
+
+        var command = new UpdateEducationDetailCommand()
+        {
+            Id = education1.Id,
+            Speciality = education2.Speciality,
+            University = education2.University,
+            StartDate = education1.StartDate,
+            EndDate = education1.EndDate,
+            UntilNow = education1.UntilNow,
+        };
+
+        var response = await _client.PutAsJsonAsync("/api/Profile/UpdateEducationDetail", command);
+
+        Assert.AreEqual(response.StatusCode, HttpStatusCode.BadRequest);
+        Assert.IsTrue((await response.Content.ReadFromJsonAsync<ProblemDetails>()).Detail.Contains("Education detail already exists"));
     }
 }
