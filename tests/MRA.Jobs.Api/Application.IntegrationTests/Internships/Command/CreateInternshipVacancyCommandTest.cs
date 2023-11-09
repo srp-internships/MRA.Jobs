9f05a433a2a3c58b9e1d6ecb9c6963686902cfd9
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-11-09 23:04:14 +0500

    add test CreateInternshipVacancyCommand_CreatingInternshipVacancyCommandWithVacancyTask

--- tests/MRA.Jobs.Api/Application.IntegrationTests/Internships/Command/CreateInternshipVacancyCommandTest.cs
+++ tests/MRA.Jobs.Api/Application.IntegrationTests/Internships/Command/CreateInternshipVacancyCommandTest.cs
@@ -32,6 +32,31 @@ public class CreateInternshipVacancyCommandTest : Testing
 
         response.Should().NotBeNull();
     }
+    [Test]
+    public async Task CreateInternshipVacancyCommand_CreatingInternshipVacancyCommandWithVacancyTask_Success()
+    {
+        RunAsAdministratorAsync();
+        var internshipVacancy = new CreateInternshipVacancyCommand
+        {
+            Title = "Cool Jobs",
+            Description = "Hello word",
+            ShortDescription = "Hi",
+            PublishDate = DateTime.Now,
+            EndDate = DateTime.Now.AddDays(2),
+            CategoryId = await AddVacancyCategory("internshipvacancy"),
+            ApplicationDeadline = DateTime.Now.AddDays(20),
+            Duration = 10,
+            Stipend = 100,
+            VacancyQuestions = new List<VacancyQuestionDto> { new VacancyQuestionDto { Question = "What is your English proficiency level?" } },
+            VacancyTasks= new List<VacancyTaskDto>() { new VacancyTaskDto { Title= "Create a function", Description= "Create a function Sum(a, b)",Template= "static class Function { //TODO: Create a function here }" } }
+        };
+
+        var response = await _httpClient.PostAsJsonAsync("/api/internships", internshipVacancy);
+
+        response.EnsureSuccessStatusCode();
+
+        response.Should().NotBeNull();
+    }
 
     async Task<Guid> AddVacancyCategory(string name)
     {
