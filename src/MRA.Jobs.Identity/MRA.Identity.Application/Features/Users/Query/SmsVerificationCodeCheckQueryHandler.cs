cfa298041160834e507b98b697ceaf07fee54868
firuz2006 <firuzisoboev@gmail.com>
2023-11-09 16:29:52 +0500

    fix test

--- src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Users/Query/SmsVerificationCodeCheckQueryHandler.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Users/Query/SmsVerificationCodeCheckQueryHandler.cs
@@ -1,25 +1,28 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MRA.Identity.Application.Common.Exceptions;
 using MRA.Identity.Application.Common.Interfaces.DbContexts;
 using MRA.Identity.Application.Contract.User.Queries;
-using MRA.Identity.Domain.Entities;
 
 namespace MRA.Identity.Application.Features.Users.Query;
-public class SmsVerificationCodeCheckQueryHandler : IRequestHandler<SmsVerificationCodeCheckQuery, SmsVerificationCodeStatus>
+
+public class
+    SmsVerificationCodeCheckQueryHandler : IRequestHandler<SmsVerificationCodeCheckQuery, SmsVerificationCodeStatus>
 {
-    private readonly UserManager<ApplicationUser> _userManager;
     private readonly IApplicationDbContext _context;
 
-    public SmsVerificationCodeCheckQueryHandler(UserManager<ApplicationUser> userManager, IApplicationDbContext context)
+    public SmsVerificationCodeCheckQueryHandler(IApplicationDbContext context)
     {
-        _userManager = userManager;
         _context = context;
     }
-    public async Task<SmsVerificationCodeStatus> Handle(SmsVerificationCodeCheckQuery request, CancellationToken cancellationToken)
+
+    public async Task<SmsVerificationCodeStatus> Handle(SmsVerificationCodeCheckQuery request,
+        CancellationToken cancellationToken)
     {
         request.PhoneNumber = request.PhoneNumber.Trim();
         if (request.PhoneNumber.Length == 9) request.PhoneNumber = "+992" + request.PhoneNumber.Trim();
-        else if (request.PhoneNumber.Length == 12 && request.PhoneNumber[0] != '+') request.PhoneNumber = "+" + request.PhoneNumber;
+        else if (request.PhoneNumber.Length == 12 && request.PhoneNumber[0] != '+')
+            request.PhoneNumber = "+" + request.PhoneNumber;
 
         var expirationTime = DateTime.Now.AddMinutes(-1);
 
@@ -28,7 +31,14 @@ public class SmsVerificationCodeCheckQueryHandler : IRequestHandler<SmsVerificat
 
         if (!result)
             return SmsVerificationCodeStatus.CodeVerifyFailure;
-
+        
+        var user =
+            await _context.Users.FirstOrDefaultAsync(s => s.PhoneNumber == request.PhoneNumber,
+                cancellationToken: cancellationToken) ??
+            throw new NotFoundException($"User with {request.PhoneNumber} phone number not found");
+        user.PhoneNumberConfirmed = true;
+        await _context.SaveChangesAsync(cancellationToken);
+        
         return SmsVerificationCodeStatus.CodeVerifySuccess;
     }
 }
