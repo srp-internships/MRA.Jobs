f6446eb13eccacce68f8c8600db488acd8d8cb51
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-11-09 14:39:30 +0500

    Create & Update Education for user check dublicate

--- src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Educations/Commands/UpdateEducationDetailCommandHandler.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Features/Educations/Commands/UpdateEducationDetailCommandHandler.cs
@@ -24,18 +24,37 @@ public class
         _mapper = mapper;
     }
 
-    public async Task<Guid> Handle(UpdateEducationDetailCommand request,
-        CancellationToken cancellationToken)
+    public async Task<Guid> Handle(UpdateEducationDetailCommand request, CancellationToken cancellationToken)
     {
         var userId = _userHttpContextAccessor.GetUserId();
         var user = await _context.Users
             .Include(u => u.Educations)
             .FirstOrDefaultAsync(u => u.Id.Equals(userId));
         _ = user ?? throw new NotFoundException("user is not found");
+
         var education = user.Educations.FirstOrDefault(e => e.Id.Equals(request.Id));
         _ = education ?? throw new NotFoundException("education not exits");
 
-        _mapper.Map(request, education);
+        var textInfo = System.Globalization.CultureInfo.CurrentCulture.TextInfo;
+        var university = textInfo.ToTitleCase(request.University.Trim());
+        var speciality = textInfo.ToTitleCase(request.Speciality.Trim());
+
+        var existingEducation = user.Educations.FirstOrDefault(e =>
+            e.Id != request.Id &&
+            e.University.Equals(university, StringComparison.OrdinalIgnoreCase) &&
+            e.Speciality.Equals(speciality, StringComparison.OrdinalIgnoreCase));
+
+        if (existingEducation != null)
+        {
+            throw new DuplicateWaitObjectException("Education detail already exists.");
+        }
+
+        education.University = university;
+        education.Speciality = speciality;
+        education.StartDate = request.StartDate.HasValue ? request.StartDate.Value : default(DateTime);
+        education.EndDate = request.EndDate.HasValue ? request.EndDate.Value : default(DateTime);
+        education.UntilNow = request.UntilNow;
+
         await _context.SaveChangesAsync();
         return education.Id;
     }
