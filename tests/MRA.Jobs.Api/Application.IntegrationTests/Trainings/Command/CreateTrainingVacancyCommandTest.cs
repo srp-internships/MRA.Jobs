68118e7b3574ad47ebebc2414890c4e91ac19f2a
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-11-09 23:13:36 +0500

    add test CreateTrainingVacancyCommand_CreatingTrainingVacancyWithVacancyTask

--- tests/MRA.Jobs.Api/Application.IntegrationTests/Trainings/Command/CreateTrainingVacancyCommandTest.cs
+++ tests/MRA.Jobs.Api/Application.IntegrationTests/Trainings/Command/CreateTrainingVacancyCommandTest.cs
@@ -31,7 +31,34 @@ public class CreateTrainingVacancyCommandTest : Testing
 
         response.Should().NotBeNull();
     }
+    [Test]
+    public async Task CreateTrainingVacancyCommand_CreatingTrainingVacancyWithVacancyTask_Success()
+    {
+        var trainingVacancy = new CreateTrainingVacancyCommand
+        {
+            Title = "Good Job!",
+            Description = "Hey dude!",
+            ShortDescription = "Hello",
+            PublishDate = DateTime.Now,
+            EndDate = DateTime.Now.AddDays(2),
+            CategoryId = await AddVacancyCategory("jobvacancy"),
+            Duration = 2,
+            VacancyQuestions = new List<VacancyQuestionDto> { new VacancyQuestionDto { Question = "What is your English proficiency level?" } },
+            VacancyTasks = new List<VacancyTaskDto>() { new VacancyTaskDto {
+            Title= "Create a function",
+            Description= "Create a function Sum(a, b)",
+            Template= "static class Function { //TODO: Create a function here }",
+            Test= "using NUnit.Framework; class FunctionTests { [Test] public void FunctionTest(){int a = 100;int b = 100;int summ = Function.Sum(a, b);Assert.AreEqual(200, summ, 0);}}" } },
+            Fees = 5
+        };
 
+        RunAsReviewerAsync();
+        var response = await _httpClient.PostAsJsonAsync("/api/trainings", trainingVacancy);
+
+        response.EnsureSuccessStatusCode();
+
+        response.Should().NotBeNull();
+    }
     async Task<Guid> AddVacancyCategory(string name)
     {
         var vacancyCategory = new VacancyCategory
