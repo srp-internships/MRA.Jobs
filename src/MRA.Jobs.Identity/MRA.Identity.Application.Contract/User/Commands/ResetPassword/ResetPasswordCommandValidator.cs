978d2b7bafdf76456829f3bf99bb8408e7e038ef
adheeeem <sherman.peabody99@gmail.com>
2023-11-14 10:21:36 +0500

    reset password regex updated

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Commands/ResetPassword/ResetPasswordCommandValidator.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Commands/ResetPassword/ResetPasswordCommandValidator.cs
@@ -5,7 +5,7 @@ public class ResetPasswordCommandValidator : AbstractValidator<ResetPasswordComm
 {
     public ResetPasswordCommandValidator()
     {
-        RuleFor(s => s.Password).Matches(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$").WithMessage("Password must have at least one alphabetical character, one digit, and be at least 8 characters long.");
+        RuleFor(s => s.Password).Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{8,}$").WithMessage("Password must have at least one lowercase letter, one uppercase letter, one digit, and one special character, with a minimum length of 8 characters.");
         RuleFor(s => s.PhoneNumber).Matches(@"^\+992\d{9}$")
             .WithMessage("Invalid phone number. Example : +992921234567");
         RuleFor(s => s.ConfirmPassword).Equal(s => s.Password).WithMessage("ConfirmPassword must be equal to Password.");
