4caf37acb8ea0ad7423a1e5dc6c929176f0abd63
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-11-16 17:01:31 +0500

    Create Document Template

---
+++ src/MRA.Jobs.Client/Services/QuestPdfServices/InvoiceDocument .cs
@@ -0,0 +1,77 @@
+﻿using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace MRA.Jobs.Client.Services.QuestPdfServices;
+
+public class InvoiceDocument : IDocument
+{
+    public InvoiceModel Model { get; }
+
+    public InvoiceDocument(InvoiceModel model)
+    {
+        Model = model;
+    }
+
+    public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+    public DocumentSettings GetSettings() => DocumentSettings.Default;
+
+    public void Compose(IDocumentContainer container)
+    {
+        container
+           .Page(page =>
+           {
+               page.Margin(50);
+
+               page.Header().Element(ComposeHeader);
+               page.Content().Element(ComposeContent);
+
+
+               page.Footer().AlignCenter().Text(x =>
+               {
+                   x.CurrentPageNumber();
+                   x.Span(" / ");
+                   x.TotalPages();
+               });
+           });
+
+        void ComposeHeader(IContainer container)
+        {
+            var titleStyle = TextStyle.Default.FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+
+            container.Row(row =>
+            {
+                row.RelativeItem().Column(column =>
+                {
+                    column.Item().Text($"Invoice #{Model.InvoiceNumber}").Style(titleStyle);
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Issue date: ").SemiBold();
+                        text.Span($"{Model.IssueDate:d}");
+                    });
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Due date: ").SemiBold();
+                        text.Span($"{Model.DueDate:d}");
+                    });
+                });
+
+                row.ConstantItem(100).Height(50).Placeholder();
+            });
+        }
+
+        void ComposeContent(IContainer container)
+        {
+            container
+                .PaddingVertical(40)
+                .Height(250)
+                .Background(Colors.Grey.Lighten3)
+                .AlignCenter()
+                .AlignMiddle()
+                .Text("Content").FontSize(16);
+        }
+    }
+}
