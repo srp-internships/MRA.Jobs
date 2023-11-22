2f335eae4f91783117a2c0c4ccdbd06d521c903d
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-11-22 09:27:18 +0500

    add ITaskService

---
+++ src/MRA.Jobs.Api/Application/Common/Interfaces/ITaskService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRA.Jobs.Application.Common.Interfaces;
+public interface ITaskService
+{
+
+}
