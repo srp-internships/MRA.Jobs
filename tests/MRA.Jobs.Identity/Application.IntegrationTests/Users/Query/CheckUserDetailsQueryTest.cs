f453fb395e364987281d501e8cd235940af329c6
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-11-23 11:02:29 +0500

    fix test

--- tests/MRA.Jobs.Identity/Application.IntegrationTests/Users/Query/CheckUserDetailsQueryTest.cs
+++ tests/MRA.Jobs.Identity/Application.IntegrationTests/Users/Query/CheckUserDetailsQueryTest.cs
@@ -5,10 +5,10 @@ using MRA.Identity.Application.Contract.User.Responses;
 namespace MRA.Jobs.Application.IntegrationTests.Users.Query;
 public class CheckUserDetailsQueryTest : BaseTest
 {
-    [TestCase("test1@example.com", "+992111111111", "@Alex123", true)]
-    [TestCase("test2@example.com", "+992222222222", "@Bob456", true)]
-    [TestCase("test3@example.com", "+992333333333", "@Charlie789", true)]
-    [TestCase("test@example.com", "+992123456789", "@Alex33", false)]
+    [TestCase("test@example.com", "+992111111111", "@Alex123", false)]
+    [TestCase("test2123@example.com", "+992522222222", "@Bob456", true)]
+    [TestCase("test31@example.com", "+992333333333", "@Charlie789", true)]
+    [TestCase("test1@example.com", "+992123456789", "@Alex33", false)]
     [TestCase("reviewer@example.com", "+992223456789", "@Reviewer", false)]
     public async Task CheckUserDetailsQuery_UserDataAvailability(string email, string phoneNumber, string userName, bool expectedResult)
     {
@@ -25,7 +25,10 @@ public class CheckUserDetailsQueryTest : BaseTest
         var result = await response.Content.ReadFromJsonAsync<UserDetailsResponse>();
 
         bool isDataAvailable = !result.IsEmailTaken && !result.IsPhoneNumberTaken && !result.IsUserNameTaken;
-        Assert.That(isDataAvailable, Is.EqualTo(expectedResult), "Data availability result is not as expected");
+        Assert.That(isDataAvailable, Is.EqualTo(expectedResult), "Data availability result is not as expected. " +
+            $" IsUserNameTaken: {result.IsUserNameTaken}" +
+            $" IsPhoneNumberTaken: {result.IsPhoneNumberTaken}" +
+            $" IsEmailTaken: {result.IsEmailTaken}");
     }
 
 }
