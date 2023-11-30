b85d0c153e10b34812f06a7f263cbddb8ea30ed0
adheeeem <sherman.peabody99@gmail.com>
2023-11-30 10:24:10 +0500

    confirm password field

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Commands/RegisterUser/RegisterUserCommand.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Commands/RegisterUser/RegisterUserCommand.cs
@@ -12,5 +12,6 @@ public class RegisterUserCommand : IRequest<Guid>
     public string Username { get; set; } = "";
     public string Password { get; set; } = "";
     public string Application { get; set; } = "MraJobs";
+    public string ConfirmPassword { get; set; }
     public string Role { get; set; } = "Applicant";
 }
