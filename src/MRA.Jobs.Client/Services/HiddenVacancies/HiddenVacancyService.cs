6b943c310230af9fee3634130002c6f4b970b58a
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-11-30 16:55:04 +0500

    Upload CV for HiddenVacancy

---
+++ src/MRA.Jobs.Client/Services/HiddenVacancies/HiddenVacancyService.cs
@@ -0,0 +1,26 @@
+﻿using MRA.Jobs.Application.Contracts.HiddenVacancies.Responses;
+using MRA.Jobs.Client.Identity;
+using MudBlazor;
+
+namespace MRA.Jobs.Client.Services.HiddenVacancies;
+
+public class HiddenVacancyService(HttpClient httpClient, ISnackbar snackbar) : IHiddenVacancyService
+{
+    public async Task<HiddenVacancyResponse> GetHiddenVacancy()
+    {
+        var vacancy = new HiddenVacancyResponse();
+        try
+        {
+            var response = await httpClient.GetAsync("hiddenvacancies");
+            if (response.IsSuccessStatusCode)
+                vacancy = await response.Content.ReadFromJsonAsync<HiddenVacancyResponse>();
+            else
+                snackbar.Add((await response.Content.ReadFromJsonAsync<CustomProblemDetails>()).Detail);
+        }
+        catch (Exception e)
+        {
+            snackbar.Add("Server is not responding, please try later", Severity.Error);
+        }
+        return vacancy;
+    }
+}
---
+++ src/MRA.Jobs.Client/Services/HiddenVacancies/IHiddenVacancyService.cs
@@ -0,0 +1,8 @@
+﻿using MRA.Jobs.Application.Contracts.HiddenVacancies.Responses;
+
+namespace MRA.Jobs.Client.Services.HiddenVacancies;
+
+public interface IHiddenVacancyService
+{
+    Task<HiddenVacancyResponse> GetHiddenVacancy();
+}
