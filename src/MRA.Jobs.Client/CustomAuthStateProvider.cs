599412590582e1a6465c00b6c2f18447ed916ed2
firuz2006 <firuzisoboev@gmail.com>
2023-11-30 10:22:52 +0500

    remove localstorage with replacing to cookies

--- src/MRA.Jobs.Client/CustomAuthStateProvider.cs
+++ src/MRA.Jobs.Client/CustomAuthStateProvider.cs
@@ -2,46 +2,35 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Net.Http.Headers;
 using System.Security.Claims;
-using System.Text.Json;
-using Blazored.LocalStorage;
+using AltairCA.Blazor.WebAssembly.Cookie;
 using MRA.Identity.Application.Contract.User.Queries;
 using MRA.Identity.Application.Contract.User.Responses;
 
 namespace MRA.Jobs.Client;
 
-public class CustomAuthStateProvider : AuthenticationStateProvider
-{
-    private readonly ILocalStorageService _localStorageService;
-    private readonly HttpClient _http;
-    private readonly IdentityHttpClient _identityHttp;
-
-    public CustomAuthStateProvider(ILocalStorageService localStorageService, HttpClient http,
+public class CustomAuthStateProvider(IAltairCABlazorCookieUtil cookieUtil, HttpClient http,
         IdentityHttpClient identityHttp)
-    {
-        _localStorageService = localStorageService;
-        _http = http;
-        _identityHttp = identityHttp;
-    }
-
+    : AuthenticationStateProvider
+{
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         var authToken = await GetTokenAsync();
         var identity = new ClaimsIdentity();
-        _http.DefaultRequestHeaders.Authorization = null;
+        http.DefaultRequestHeaders.Authorization = null;
 
         if (authToken != null && !string.IsNullOrEmpty(authToken.AccessToken))
         {
             try
             {
                 identity = new ClaimsIdentity(ParseClaimsFromJwt(authToken.AccessToken), "jwt");
-                _http.DefaultRequestHeaders.Authorization =
+                http.DefaultRequestHeaders.Authorization =
                     new AuthenticationHeaderValue("Bearer", authToken.AccessToken.Replace("\"", ""));
-                _identityHttp.DefaultRequestHeaders.Authorization =
+                identityHttp.DefaultRequestHeaders.Authorization =
                     new AuthenticationHeaderValue("Bearer", authToken.AccessToken.Replace("\"", ""));
             }
             catch
             {
-                await _localStorageService.RemoveItemAsync("authToken");
+                await cookieUtil.RemoveAsync("authToken");
                 identity = new ClaimsIdentity();
             }
         }
@@ -56,7 +45,7 @@ public class CustomAuthStateProvider : AuthenticationStateProvider
 
     private async Task<JwtTokenResponse> GetTokenAsync()
     {
-        var token = await _localStorageService.GetItemAsync<JwtTokenResponse>("authToken");
+        var token = await cookieUtil.GetValueAsync<JwtTokenResponse>("authToken");
         if (token == null)
         {
             return null;
@@ -64,7 +53,7 @@ public class CustomAuthStateProvider : AuthenticationStateProvider
 
         if (token.AccessTokenValidateTo <= DateTime.Now)
         {
-            var refreshResponse = await _identityHttp.PostAsJsonAsync("auth/refresh",
+            var refreshResponse = await identityHttp.PostAsJsonAsync("auth/refresh",
                 new GetAccessTokenUsingRefreshTokenQuery
                 {
                     RefreshToken = token.RefreshToken, AccessToken = token.AccessToken
@@ -75,12 +64,12 @@ public class CustomAuthStateProvider : AuthenticationStateProvider
             }
 
             token = await refreshResponse.Content.ReadFromJsonAsync<JwtTokenResponse>();
-            await _localStorageService.SetItemAsync("authToken", token);
+            await cookieUtil.SetValueAsync("authToken", token);
         }
 
         return token;
     }
-    
+
     private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
     {
         var jwtObject = new JwtSecurityTokenHandler().ReadJwtToken(jwt);
