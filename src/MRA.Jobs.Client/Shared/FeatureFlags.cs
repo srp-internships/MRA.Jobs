cd6a2471609e46a865b028407865e9f3b8677127
adheeeem <sherman.peabody99@gmail.com>
2023-12-20 09:28:35 +0500

    beta version feature flag

--- src/MRA.Jobs.Client/Shared/FeatureFlags.cs
+++ src/MRA.Jobs.Client/Shared/FeatureFlags.cs
@@ -3,5 +3,5 @@ namespace MRA.Jobs.Client.Shared;
 public static class FeatureFlags
 {
     public const string ExternalAuthorization="ExternalAuthorization";
-    public const string Version = "Beta";
+    public const string BetaVersion = "BetaVersion";
 }
