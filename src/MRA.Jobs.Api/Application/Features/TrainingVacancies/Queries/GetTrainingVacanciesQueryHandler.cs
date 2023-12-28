1b44a2940cebe35c1e39320ba0f3740740ed8043
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2023-12-28 14:41:53 +0500

    Commit

--- src/MRA.Jobs.Api/Application/Features/TrainingVacancies/Queries/GetTrainingVacanciesQueryHandler.cs
+++ src/MRA.Jobs.Api/Application/Features/TrainingVacancies/Queries/GetTrainingVacanciesQueryHandler.cs
@@ -6,26 +6,18 @@ using MRA.Jobs.Application.Contracts.TrainingVacancies.Responses;
 
 namespace MRA.Jobs.Application.Features.TrainingVacancies.Queries;
 
-public class
-    GetTrainingVacanciesQueryHandler : IRequestHandler<GetTrainingsQueryOptions,
+public class GetTrainingVacanciesQueryHandler(
+        IApplicationDbContext context,
+        IMapper mapper,
+        IApplicationSieveProcessor sieveProcessor,
+        IUserHttpContextAccessor userHttpContextAccessor)
+    : IRequestHandler<GetTrainingsQueryOptions,
         PagedList<TrainingVacancyListDto>>
 {
-    private readonly IApplicationDbContext _context;
-    private readonly IMapper _mapper;
-    private readonly IApplicationSieveProcessor _sieveProcessor;
-
-    public GetTrainingVacanciesQueryHandler(IApplicationDbContext context, IMapper mapper,
-        IApplicationSieveProcessor sieveProcessor)
-    {
-        _context = context;
-        _mapper = mapper;
-        _sieveProcessor = sieveProcessor;
-    }
-
     public async Task<PagedList<TrainingVacancyListDto>> Handle(GetTrainingsQueryOptions request,
         CancellationToken cancellationToken)
     {
-        var trainings = (await _context.TrainingVacancies
+        var trainings = (await context.TrainingVacancies
             .Include(t => t.Category)
             .Include(t => t.VacancyQuestions)
             .Include(i => i.VacancyTasks)
@@ -41,14 +33,18 @@ public class
             trainings = trainings.Where(t => t.Title.ToLower().Trim().Contains(request.SearchText.ToLower().Trim()));
         }
 
+        var userRoles = userHttpContextAccessor.GetUserRoles();
+        if (userRoles.Any(role => role == "Applicant"))
+            request.CheckDate = true;
+        
         if (request.CheckDate)
         {
             DateTime now = DateTime.Now;
             trainings = trainings.Where(t => t.PublishDate <= now && t.EndDate >= now);
         }
 
-        PagedList<TrainingVacancyListDto> result = _sieveProcessor.ApplyAdnGetPagedList(request,
-            trainings.AsQueryable(), _mapper.Map<TrainingVacancyListDto>);
+        PagedList<TrainingVacancyListDto> result = sieveProcessor.ApplyAdnGetPagedList(request,
+            trainings.AsQueryable(), mapper.Map<TrainingVacancyListDto>);
         return await Task.FromResult(result);
     }
 }
