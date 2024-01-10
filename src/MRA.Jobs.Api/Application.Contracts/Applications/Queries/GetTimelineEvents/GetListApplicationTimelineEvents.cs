b540135aa31924371405ac73bc85117475ab2a43
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-10 14:04:21 +0500

    GetListApplicationTimelineEventsQueryHandler

---
+++ src/MRA.Jobs.Api/Application.Contracts/Applications/Queries/GetTimelineEvents/GetListApplicationTimelineEvents.cs
@@ -0,0 +1,9 @@
+﻿using MRA.Jobs.Application.Contracts.TimeLineDTO;
+
+namespace MRA.Jobs.Application.Contracts.Applications.Queries.GetTimelineEvents
+;
+
+public class GetListApplicationTimelineEventsQuery : IRequest<List<TimeLineDetailsDto>>
+{
+    public string ApplicationSlug { get; set; }
+}
---
+++ src/MRA.Jobs.Api/Application.Contracts/Applications/Queries/GetTimelineEvents/GetListApplicationTimelineEventsValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace MRA.Jobs.Application.Contracts.Applications.Queries.GetTimelineEvents;
+
+public class GetListApplicationTimelineEventsQueryValidator : AbstractValidator<GetListApplicationTimelineEventsQuery>
+{
+    public GetListApplicationTimelineEventsQueryValidator()
+    {
+        RuleFor(x => x.ApplicationSlug).NotEmpty();
+    }
+}
