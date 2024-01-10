6e30886fcb2da96d36fcc376e76bba422a492bf0
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-10 14:10:57 +0500

    add method GetTimelineEvents for ApplicationsController

--- src/MRA.Jobs.Api/WebUI/Controllers/ApplicationsController.cs
+++ src/MRA.Jobs.Api/WebUI/Controllers/ApplicationsController.cs
@@ -7,6 +7,7 @@ using MRA.Jobs.Application.Contracts.Applications.Commands.Delete;
 using MRA.Jobs.Application.Contracts.Applications.Commands.UpdateApplication;
 using MRA.Jobs.Application.Contracts.Applications.Commands.UpdateApplicationStatus;
 using MRA.Jobs.Application.Contracts.Applications.Queries.GetApplicationBySlug;
+using MRA.Jobs.Application.Contracts.Applications.Queries.GetTimelineEvents;
 using MRA.Jobs.Application.Contracts.Applications.Responses;
 using MRA.Jobs.Application.Contracts.Common;
 using MRA.Jobs.Application.Contracts.TimeLineDTO;
@@ -90,5 +91,12 @@ public class ApplicationsController(IFileService fileService) : ApiControllerBas
         CancellationToken cancellationToken)
     {
         return await Mediator.Send(request, cancellationToken);
+    }
+
+    [HttpGet("GetTimelineEvents/{slug}")]
+    [Authorize(policy: ApplicationPolicies.Reviewer)]
+    public async Task<ActionResult<List<TimeLineDetailsDto>>> GetTimelineEvents([FromQuery] string slug)
+    {
+        return await Mediator.Send(new GetListApplicationTimelineEventsQuery() { ApplicationSlug = slug });
     }
 }
