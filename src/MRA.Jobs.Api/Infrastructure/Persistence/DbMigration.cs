4c6fc4aad8b9f367a0b5a92d029f0773b154eeb9
adheeeem <sherman.peabody99@gmail.com>
2024-01-10 11:23:58 +0500

    staging environment

--- src/MRA.Jobs.Api/Infrastructure/Persistence/DbMigration.cs
+++ src/MRA.Jobs.Api/Infrastructure/Persistence/DbMigration.cs
@@ -1,15 +1,21 @@
-﻿namespace MRA.Jobs.Infrastructure.Persistence;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MRA.Jobs.Infrastructure.Persistence;
 public class DbMigration
 {
     private readonly ApplicationDbContext _context;
+    private readonly IConfiguration _configuration;
 
-    public DbMigration(ApplicationDbContext context)
+    public DbMigration(ApplicationDbContext context, IConfiguration configuration)
     {
         _context = context;
+        _configuration = configuration;
     }
 
     public async Task InitialiseAsync()
     {
+        if (_configuration["Environment"] == "Staging") _context.Database.EnsureDeleted();
+
         if (_context.Database.IsSqlServer())
         {
             await _context.Database.MigrateAsync();
