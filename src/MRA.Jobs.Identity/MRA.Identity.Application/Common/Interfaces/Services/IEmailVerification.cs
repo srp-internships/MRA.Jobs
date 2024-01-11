9fcb9cfc9c0db58984b0bca683bf8e2ef9efe36c
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-11 12:01:56 +0500

    Remove EmailApproved & EmailRejected from Mra.Identity EmailVerification

--- src/MRA.Jobs.Identity/MRA.Identity.Application/Common/Interfaces/Services/IEmailVerification.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application/Common/Interfaces/Services/IEmailVerification.cs
@@ -6,6 +6,4 @@ public interface IEmailVerification
 {
     Task<bool> SendVerificationEmailAsync(ApplicationUser user);
     Task VerifyEmailAsync(string token, Guid userId);
-    Task<bool> EmailApproved(ApplicationUser user);
-    Task<bool> EmailRejected(ApplicationUser user);
 }
