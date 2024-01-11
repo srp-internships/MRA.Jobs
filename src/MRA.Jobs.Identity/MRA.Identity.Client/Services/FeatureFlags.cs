0af3ab2234f73dec677b514f9b3c38b6153c3d06
firuz2006 <firuzisoboev@gmail.com>
2024-01-11 17:16:19 +0500

    identity

--- src/MRA.Jobs.Identity/MRA.Identity.Client/Services/FeatureFlags.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Client/Services/FeatureFlags.cs
@@ -4,4 +4,7 @@ public static class FeatureFlags
 {
     public const string ExternalAuthorization = "ExternalAuthorization";
     public const string BetaVersion = "BetaVersion";
+    public const string En = "En";
+    public const string Ru = "Ru";
+    public const string Tj = "Tj";
 }
