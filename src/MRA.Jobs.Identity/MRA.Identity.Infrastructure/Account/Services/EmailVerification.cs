36c6cb979563c8a905e28d5473540cd95384fd57
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-12 15:38:46 +0500

    email verification template

--- src/MRA.Jobs.Identity/MRA.Identity.Infrastructure/Account/Services/EmailVerification.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Infrastructure/Account/Services/EmailVerification.cs
@@ -30,61 +30,74 @@ public class EmailVerification : IEmailVerification
         var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
         var emailBody =
             $@"    
-<center>
-  <table
-    class=""body-wrap""
-    style=""
-      text-align: center;
-      width: 86%;
-      font-family: arial, sans-serif;
-      border-spacing: 4px 14px;
-    ""
-  >
+
+<body style='background-color: #e9ecef;'>
+  <table border='0' cellpadding='0' cellspacing='0' width='100%'>
     <tr>
-      <img
-        src=""https://jobs.srp.tj/images/srp/srp_icon.png""
-        style=""width: 20%""
-      />
-      <td>
-        <center>
-          <table bgcolor=""#FFFFFF"" width=""80%"" border=""0"">
-            <tbody>
-              <tr style=""font-size: 14px"">
-                <td>Здравствуйте!</td>
-              </tr>
-              <tr style=""font-size: 14px"">
-                <td>
-                  Благодарим вас за регистрацию на нашем сайте. Мы рады, что вы
-                  присоединились к нашему сообществу.
+      <td align='center' bgcolor='#e9ecef'>
+        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
+            <tr>
+                <td align='center' valign='top' style='padding: 36px 24px;'>
+                  <a href='https://www.blogdesire.com' target='_blank' style='display: inline-block;'>
+                    <img   src='https://jobs.srp.tj/images/srp/srp_icon.png'
+                    style='width: 30%' alt='Logo' border='0' style='display: block; width: 48px; max-width: 48px; min-width: 48px;'>
+                  </a>
                 </td>
               </tr>
-              <tr style=""font-size: 14px"">
-                <td>
-                  Для завершения регистрации и активации вашей учетной записи
-                  вам необходимо подтвердить ваш адрес электронной почты.
-                </td>
-              </tr>
-              <tr style=""font-size: 14px"">
-                <td>
-                 <b> Для
-                  этого просто перейдите по ссылке: <a
-                  href='{{_configurations[""MraIdentityClient-HostName""]}}/verifyEmail?token={{WebUtility.UrlEncode(token)}}&userId={{user.Id}}'
-                  >Подтвердить электронную почту</a></b>
-                </td>
-              </tr>
-              <tr style=""font-size: 14px"">
-                <td>
-                  Если вы не регистрировались на нашем сайте, пожалуйста,
-                  проигнорируйте это письмо.
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </center>
+          <tr>
+            <td align='left' bgcolor='#ffffff' style='padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;'>
+              <h1 style='margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;'>Подтвердите свой адрес электронной почты</h1>
+            </td>
+          </tr>
+        </table>
       </td>
     </tr>
-  </table>
-</center>
+
+    <tr>
+      <td align='center' bgcolor='#e9ecef'>
+        <table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width: 600px;'>
+          <tr>
+            <td align='left' bgcolor='#ffffff' style='padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;'>
+              <p style='margin: 0;'>Нажмите кнопку ниже, чтобы подтвердить свой адрес электронной почты.</p>
+            </td>
+          </tr>
+          <tr>
+            <td align='left' bgcolor='#ffffff'>
+              <table border='0' cellpadding='0' cellspacing='0' width='100%'>
+                <tr>
+                  <td bgcolor='#ffffff' style='padding: 12px;'>
+                    <table border='0' cellpadding='0' cellspacing='0'>
+                      <tr>
+                        <td bgcolor='#1a82e2' style='border-radius: 6px;'>
+                          <a href='{_configurations["MraIdentityClient-HostName"]}/verifyEmail?token={WebUtility.UrlEncode(token)}&userId={user.Id}' style='display: inline-block; padding: 16px 36px; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;'>Подтвердить</a>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+       
+          <tr>
+            <td align='left' bgcolor='#ffffff' style='padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;'>
+              <p style='margin: 0;'>Если вы не создавали учетную запись с помощью, вы можете безопасно удалить это письмо.</p>
+            </td>
+          </tr>
+       
+          <tr>
+            <td align='left' bgcolor='#ffffff' style='padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf'>
+              <p style='margin: 0;'></p>
+            </td>
+          </tr>
+        
+        </table>
+    
+      </td>
+    </tr> 
+    </table>
+</body>
+
 ";
         var emailSubject = "Email Verification";
         return await _emailService.SendEmailAsync(new[] { user.Email }, emailBody, emailSubject);
