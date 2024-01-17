eec1f5f340b663dc6c56585310a4dab7537352c6
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-17 14:07:54 +0500

    commit

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Commands/RegisterUser/RegisterUserCommandValidator.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/User/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -11,8 +11,9 @@ public class RegisterUserCommandValidator : AbstractValidator<RegisterUserComman
         RuleFor(s=>s.FirstName).NotEmpty();
         RuleFor(s=>s.LastName).NotEmpty();
         RuleFor(s => s.PhoneNumber).Matches(@"^(?:\d{9}|\+992\d{9}|992\d{9})$").WithMessage("Invalid phone number. Example : +992921234567, 992921234567, 921234567");
-        RuleFor(s => s.Username.Trim()).MinimumLength(4)
-            .Must(username => username.All(c => char.IsLetterOrDigit(c) || c == '_' || c == '@'))
+        RuleFor(s => s.Username.Trim());
+        RuleFor(s => s.Username).MinimumLength(5)
+            .Must(username => username.Trim().All(c => char.IsLetterOrDigit(c) || c == '_' || c == '@'))
             .WithMessage("Username should only consist of letters, numbers, underscores, or the @ symbol.");
         
         RuleFor(s => s.Password).NotEmpty().MinimumLength(5);
