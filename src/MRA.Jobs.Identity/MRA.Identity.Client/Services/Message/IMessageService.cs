bd5398584ddd336365a574ed3d66f05ac20f31b2
adheeeem <sherman.peabody99@gmail.com>
2024-01-18 10:18:05 +0500

    few changes on client side

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Client/Services/Message/IMessageService.cs
@@ -0,0 +1,8 @@
+﻿using MRA.Identity.Application.Contract.Messages.Commands;
+
+namespace MRA.Identity.Client.Services.Message;
+
+public interface IMessageService
+{
+    Task<HttpResponseMessage> SendMessageAsync(SendMessageCommand command);
+}
---
+++ src/MRA.Jobs.Identity/MRA.Identity.Client/Services/Message/MessageService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using MRA.Identity.Application.Contract.Messages.Commands;
+
+namespace MRA.Identity.Client.Services.Message;
+
+public class MessageService : IMessageService
+{
+    private readonly HttpClient _httpClient;
+
+    public MessageService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<HttpResponseMessage> SendMessageAsync(SendMessageCommand command)
+    {
+        var result = await _httpClient.PostAsJsonAsync("message", command);
+        return result;
+    }
+}
