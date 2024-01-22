0379e036d3cad9e4e28ff4d2aa72402317a1629e
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-01-22 22:32:30 +0500

    Revert "delete RolesController"
    This reverts commit ed116d5c36e677e11d9e0992ba4472569a246a91.

---
+++ src/MRA.Jobs.Identity/MRA.Identity.Api/Controllers/RolesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using MRA.Identity.Application.Contract.ApplicationRoles.Queries;
+using MRA.Identity.Application.Contract.ApplicationRoles.Commands;
+
+namespace MRA.Identity.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(ApplicationPolicies.Administrator)]
+public class RolesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public RolesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var query = new GetRolesQuery();
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+
+    [HttpGet("{slug}")]
+    public async Task<IActionResult> Get(string slug)
+    {
+        var query = new GetRoleBySlugQuery { Slug = slug };
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateRoleCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpPut("{slug}")]
+    public async Task<IActionResult> Update(UpdateRoleCommand command, string slug)
+    {
+        command.Slug = slug;
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpDelete("{slug}")]
+    public async Task<IActionResult> Delete(string slug)
+    {
+        var command = new DeleteRoleCommand { Slug = slug };
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
