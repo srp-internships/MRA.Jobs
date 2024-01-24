abf34387745c9449b1997b67978f56fc9493b2ce
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-01-24 21:44:27 +0500

    change to IHttpClientServiceBase

--- src/MRA.Jobs.Identity/MRA.Identity.Client/Services/HttpClients/IHttpClientService.cs
+++ src/MRA.Jobs.Identity/MRA.Identity.Client/Services/HttpClients/IHttpClientService.cs
@@ -1,5 +1,12 @@
 ﻿namespace MRA.Identity.Client.Services.HttpClients;
 
-public class IHttpClientService
+public interface IHttpClientServiceBase
 {
+    Task<ApiResponse<T>> PostAsJsonAsync<T>(string url, object content);
+
+    Task<ApiResponse<T>> GetAsJsonAsync<T>(string url, object content = null);
+
+    Task<ApiResponse<T>> PutAsJsonAsync<T>(string url, object content);
+
+    Task<ApiResponse> DeleteAsync(string url);
 }
