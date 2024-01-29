134a67c5ba223eacb6b9d4bc9f5d3f30357d8ceb
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-29 10:25:34 +0500

    remove Identity.Application.Contract

--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/ApplicationRoles/Commands/UpdateRoleCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace MRA.Identity.Application.Contract.ApplicationRoles.Commands;
-public class UpdateRoleCommand : IRequest<Guid>
-{
-    public string Slug { get; set; }
-    public string NewRoleName { get; set; }
-}
--- src/MRA.Jobs.Identity/MRA.Identity.Application.Contract/UserRoles/Commands/CreateUserRolesCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace MRA.Identity.Application.Contract.UserRoles.Commands;
-public class CreateUserRolesCommand : IRequest<string>
-{
-    public string RoleName{ get; set; }
-    public string UserName { get; set; }
-
-}
