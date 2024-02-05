51795ad3a6ca8ace56a5ef2ed043e74c4db36142
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-02-05 19:03:26 +0500

    test

--- tests/MRA.Jobs.Api/Application.IntegrationTests/Application/GetApplicationsPagedQueryTest.cs
+++ tests/MRA.Jobs.Api/Application.IntegrationTests/Application/GetApplicationsPagedQueryTest.cs
@@ -9,66 +9,58 @@ namespace MRA.Jobs.Application.IntegrationTests.Application;
 
 public class GetApplicationsPagedQueryTest : CreateApplicationTestsBase
 {
-    private  async Task CreateApplications(string jobTitle)
+    private async Task CreateApplications(string jobTitle)
     {
         var vacancySlug1 = await AddJobVacancyAsync(jobTitle);
         var testSubmit1 = new CreateApplicationCommand
         {
             VacancySlug = vacancySlug1,
             CoverLetter = RandomString(150),
-            Cv =
-            {
-                IsUploadCvMode = true,
-                CvBytes = new byte[] { 1, 2, 3 },
-                FileName = "213.bytes"
-            }
+            Cv = { IsUploadCvMode = true, CvBytes = new byte[] { 1, 2, 3 }, FileName = "213.bytes" }
         };
         RunAsDefaultUserAsync("applicant1");
         await _httpClient.PostAsJsonAsync(ApplicationApiEndPoint, testSubmit1);
 
-        
+
         var vacancySlug2 = await AddJobVacancyAsync(jobTitle);
         var testSubmit2 = new CreateApplicationCommand
         {
             VacancySlug = vacancySlug2,
             CoverLetter = RandomString(150),
-            Cv =
-            {
-                IsUploadCvMode = true,
-                CvBytes = new byte[] { 1, 2, 3 },
-                FileName = "213.bytes"
-            }
+            Cv = { IsUploadCvMode = true, CvBytes = new byte[] { 1, 2, 3 }, FileName = "213.bytes" }
         };
         RunAsDefaultUserAsync("applicant2");
         await _httpClient.PostAsJsonAsync(ApplicationApiEndPoint, testSubmit2);
     }
-    
+
     [Test]
     public async Task GetApplications_OnlyApplicantRole_ReturnsFilteredApplications()
     {
-      await  CreateApplications("JobVacancyTest1");
-        
-      RunAsDefaultUserAsync("applicant1");
-      
+        ResetState();
+        await CreateApplications("JobVacancyTest1");
+
+        RunAsDefaultUserAsync("applicant1");
+
         var response = await _httpClient.GetAsync(ApplicationApiEndPoint);
-        
-        response.EnsureSuccessStatusCode(); 
+
+        response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadFromJsonAsync<PagedList<ApplicationListDto>>();
         Assert.That(result, Is.Not.Null);
         Assert.That(result.Items, Is.Not.Empty);
         Assert.That(result.Items.All(a => a.Username == "applicant1"));
+        Assert.That(result.Items.Count == 1);
     }
-    
+
     [Test]
     public async Task GetApplications_OnlyReviewerRole_ReturnsAllApplications()
     {
-        await  CreateApplications("JobVacancyTest1");
-        
+        await CreateApplications("JobVacancyTest1");
+
         RunAsReviewerAsync();
 
         var response = await _httpClient.GetAsync(ApplicationApiEndPoint);
-        
-        response.EnsureSuccessStatusCode(); 
+
+        response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadFromJsonAsync<PagedList<ApplicationListDto>>();
         Assert.That(result, Is.Not.Null);
         Assert.That(result.Items, Is.Not.Empty);
