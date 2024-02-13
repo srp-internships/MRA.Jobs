2c0663d77052399d07688c422ebb6327164b702e
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-02-13 17:26:45 +0500

    add VacancyService

---
+++ src/MRA.Jobs.Client/Services/VacancyServices/IVacancyService.cs
@@ -0,0 +1,9 @@
+﻿using MRA.BlazorComponents.HttpClient.Responses;
+using MRA.Jobs.Application.Contracts.Vacancies.Tags.Commands;
+
+namespace MRA.Jobs.Client.Services.VacancyServices;
+
+public interface IVacancyService
+{
+    Task<ApiResponse<List<string>>> AddTagsToVacancyAsync(AddTagsToVacancyCommand command);
+}
---
+++ src/MRA.Jobs.Client/Services/VacancyServices/VacancyService.cs
@@ -0,0 +1,18 @@
+﻿using MRA.BlazorComponents.Configuration;
+using MRA.BlazorComponents.HttpClient.Responses;
+using MRA.BlazorComponents.HttpClient.Services;
+using MRA.Jobs.Application.Contracts.Vacancies.Tags.Commands;
+using MudBlazor;
+
+namespace MRA.Jobs.Client.Services.VacancyServices;
+
+public class VacancyService(IHttpClientService httpClientService, IConfiguration configuration, ISnackbar snackbar) : IVacancyService
+{
+    public async Task<ApiResponse<List<string>>> AddTagsToVacancyAsync(AddTagsToVacancyCommand command)
+    {
+        var response =
+            await httpClientService.PostAsJsonAsync<List<string>>(configuration.GetJobsUrl($"vacancies/{command.VacancyId}/tags"),
+                command);
+        return response.Success ? response : null;
+    }
+}
