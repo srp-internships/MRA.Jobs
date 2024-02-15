b6397776d685841b243d214d89626e52400c4e2b
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-02-15 16:02:17 +0500

    add TagsController

---
+++ src/MRA.Jobs.Api/WebUI/Controllers/TagsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MRA.Jobs.Application.Contracts.Tags;
+
+namespace MRA.Jobs.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await Mediator.Send(new GetTagsQuery()));
+        }
+        
+    }
+}
