acbb2ef69610d445426eef2a98f8153739aca9d6
firuz2006 <firuzisoboev@gmail.com>
2024-02-19 15:31:36 +0500

    commit

--- src/MRA.Jobs.Api/Application/Features/JobVacancies/queries/GetJobs/GetJobsQueryOptionsHandler.cs
+++ src/MRA.Jobs.Api/Application/Features/JobVacancies/queries/GetJobs/GetJobsQueryOptionsHandler.cs
@@ -21,15 +21,15 @@ public class GetJobsQueryOptionsHandler(
                 .Include(j => j.Category)
                 .Include(j => j.VacancyQuestions)
                 .Include(i => i.VacancyTasks)
-                .Include(j=>j.Tags)
-                .ThenInclude(t=>t.Tag)
+                .Include(j => j.Tags)
+                .ThenInclude(t => t.Tag)
                 .Where(j => j.Slug != CommonVacanciesSlugs.NoVacancySlug).AsNoTracking()
                 .ToListAsync(cancellationToken: cancellationToken))
             .AsEnumerable();
-        
+
         if (request.CategorySlug is not null)
             jobs = jobs.Where(t => t.Category.Slug == request.CategorySlug);
-        
+
         if (request.SearchText is not null)
             jobs = jobs.Where(t => t.Title.ToLower().Trim().Contains(request.SearchText.ToLower().Trim()));
 
@@ -47,31 +47,38 @@ public class GetJobsQueryOptionsHandler(
 
         if (request.Tags is not null && request.Tags.Count > 0)
         {
+            var requestTags = request.Tags.ToArray();
             var newList = new List<JobVacancy>();
             jobs = jobs.Where(j => j.Tags != null && j.Tags.Count != 0);
             foreach (var jobVacancy in jobs)
             {
-                var allRight = false;
-                foreach (var tag in jobVacancy.Tags)
-                {
-                    if (request.Tags.FirstOrDefault(s=>s==tag.Tag.Name) != null)
-                    {
-                        allRight = true;
-                    }
-                }
-                if (allRight)
+                var vacancyTags = jobVacancy.Tags.Select(s => s.Tag.Name).ToArray();
+                if (tagExist(vacancyTags, requestTags))
                 {
                     newList.Add(jobVacancy);
                 }
-
-               
             }
+        
             jobs = newList;
-        // jobs = jobs.Where(j => j.Tags.Any(t => request.Tags.Contains(t.Tag.Name)));
+            // jobs = jobs.Where(j => j.Tags.Any(t => request.Tags.Contains(t.Tag.Name)));
         }
 
         PagedList<JobVacancyListDto> result = sieveProcessor.ApplyAdnGetPagedList(request,
             jobs.AsQueryable(), mapper.Map<JobVacancyListDto>);
         return await Task.FromResult(result);
+    }
+
+    private bool tagExist(string[] vacancyTags, string[] requestTags)
+    {
+        var vacancyTagsJoin = vacancyTags.Aggregate("", (o, s) => o + s);
+        foreach (var requestTag in requestTags)
+        {
+            if (!vacancyTagsJoin.Contains(requestTag))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
