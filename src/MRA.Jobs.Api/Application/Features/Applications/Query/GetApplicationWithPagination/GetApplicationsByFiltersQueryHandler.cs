cecc1602f81f811f702a1d51574dc344513feb9c
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-02-26 19:11:30 +0500

    commit

---
+++ src/MRA.Jobs.Api/Application/Features/Applications/Query/GetApplicationWithPagination/GetApplicationsByFiltersQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MRA.Jobs.Application.Contracts.Applications.Queries.GetApplicationWithPagination;
+using MRA.Jobs.Application.Contracts.Applications.Responses;
+using MRA.Jobs.Application.Contracts.Common;
+
+namespace MRA.Jobs.Application.Features.Applications.Query.GetApplicationWithPagination;
+
+public class GetApplicationsByFiltersQueryHandler(IApplicationDbContext dbContext, 
+    IHttpClientFactory clientFactory,
+    IConfiguration configuration)
+    : IRequestHandler<GetApplicationsByFiltersQuery, PagedList<ApplicationListDto>>
+{
+    public async Task<PagedList<ApplicationListDto>> Handle(GetApplicationsByFiltersQuery request,
+        CancellationToken cancellationToken)
+    {
+        var application = dbContext.Applications
+            .Include(a => a.Vacancy)
+            .AsNoTracking();
+
+        var httpClient= clientFactory.CreateClient("Mra.Identity");
+        httpClient.BaseAddress =new Uri(configuration[""]);
+
+    }
+}
