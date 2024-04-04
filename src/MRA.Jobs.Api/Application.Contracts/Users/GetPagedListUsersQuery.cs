6082778fb85ddb26874f50cd2afcc8c2a9e65eed
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-04-04 11:45:23 +0500

    add GetPagedListUsersQuery

---
+++ src/MRA.Jobs.Api/Application.Contracts/Users/GetPagedListUsersQuery.cs
@@ -0,0 +1,13 @@
+﻿using MRA.Identity.Application.Contract.User.Responses;
+using MRA.Jobs.Application.Contracts.Common;
+
+namespace MRA.Jobs.Application.Contracts.Users;
+
+public class GetPagedListUsersQuery : IRequest<PagedList<UserResponse>>
+{
+    public string Filters { get; set; }
+    public string Sorts { get; set; }
+    public int? Page { get; set; }
+    public int? PageSize { get; set; }
+    public string Skills { get; set; }
+}
--- src/MRA.Jobs.Api/WebUI/Controllers/UserController.cs
+++ src/MRA.Jobs.Api/WebUI/Controllers/UserController.cs
@@ -1,37 +1,21 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using MRA.Identity.Application.Contract.User.Commands.UsersByApplications;
-using MRA.Identity.Application.Contract.User.Queries;
 using MRA.Identity.Application.Contract.User.Responses;
 using MRA.Jobs.Application.Contracts.Common;
+using MRA.Jobs.Application.Contracts.Users;
 using MRA.Jobs.Infrastructure.Identity;
 
-namespace MRA.Jobs.Web.Controllers
+namespace MRA.Jobs.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(ApplicationPolicies.Reviewer)]
+public class UserController : ApiControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize(ApplicationPolicies.Reviewer)]
-    public class UserController(IConfiguration configuration, IHttpClientFactory httpClientFactory) : ControllerBase
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] GetPagedListUsersQuery query)
     {
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] GetPagedListUsersQuery query)
-        {
-            var httpClient = httpClientFactory.CreateClient("mra-jobs");
-            var command = new GetPagedListUsersCommand()
-            {
-                ApplicationId = Guid.Parse(configuration["Application:Id"]),
-                ApplicationClientSecret = configuration["Application:ClientSecret"],
-                Filters = query.Filters,
-                Skills = query.Skills,
-                Sorts = query.Sorts,
-                PageSize = query.PageSize,
-                Page = query.Page
-            };
-            var response = await httpClient.PostAsJsonAsync(
-                configuration["MraJobs-IdentityApi:Users"],
-                command);
-            var result = await response.Content.ReadFromJsonAsync<PagedList<UserResponse>>();
-            return Ok(result);
-        }
+        var result = await Mediator.Send(query);
+        return Ok(result);
     }
 }
