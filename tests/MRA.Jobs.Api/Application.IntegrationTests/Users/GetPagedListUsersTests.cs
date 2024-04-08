6c4bc30398a221360437f8c32646fda020253de8
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-04-08 13:17:36 +0500

    GetPagedListUsersByFilter

--- tests/MRA.Jobs.Api/Application.IntegrationTests/Users/GetPagedListUsersTests.cs
+++ tests/MRA.Jobs.Api/Application.IntegrationTests/Users/GetPagedListUsersTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net.Http.Json;
+using System.Web;
 using MRA.Identity.Application.Contract.User.Responses;
 using MRA.Jobs.Application.Contracts.Common;
+using MRA.Jobs.Application.Contracts.Users;
 using NUnit.Framework;
 
 namespace MRA.Jobs.Application.IntegrationTests.Users;
@@ -12,7 +14,19 @@ public class GetPagedListUsersTests : Testing
     {
         RunAsReviewerAsync();
         var response = await _httpClient.GetFromJsonAsync<PagedList<UserResponse>>("api/user");
-
-        Assert.That(response.Items.Count==15);
+        Assert.That(response.Items.Count == 15);
     }
+
+    [Test]
+    public async Task GetPagedListUsersByFilter_ReturnPagedListUserResponse()
+    {
+        var query = new GetPagedListUsersQuery() { Filters = "FullName@=Full Name 2" };
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+        queryString["Filters"] = query.Filters;
+        RunAsReviewerAsync();
+        var response = await _httpClient.GetFromJsonAsync<PagedList<UserResponse>>($"api/user?{queryString}");
+        Assert.That(response.Items.Count == 1);
+        Assert.That(response.Items[0].UserName == "User2");
+    }
+
 }
