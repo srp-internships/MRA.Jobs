73254821a7f2f8d5ce9edd8cd0017c857a06df08
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-04-11 10:20:51 +0500

    fix user navigation

--- src/MRA.Jobs.Client/Enums/DocPages.cs
+++ src/MRA.Jobs.Client/Enums/DocPages.cs
@@ -3,20 +3,20 @@
 public static class DocPages
 {
     public const string Home = "home";
-    public const string Jobs = "jobs";
-    public const string Internships = "internships";
-    public const string Trainings = "trainings";
-    public const string Team = "team";
-    public const string Academy = "academy";
-    public const string Contact = "contact";
-    public const string Profile = "profile";
-    public const string Applications = "applications";
-    public const string NoVacancyUploadCv = "upload-cv";
-    public const string Categories = "categories";
-    public const string UserManager = "userManager";
-    public const string Candidates = "candidates";
-    public const string Users = "Users";
-    public const string EmailTemplates = "EmailTemplates";
+    public const string Jobs = "/jobs";
+    public const string Internships = "/internships";
+    public const string Trainings = "/trainings";
+    public const string Team = "/team";
+    public const string Academy = "/academy";
+    public const string Contact = "/contact";
+    public const string Profile = "/profile";
+    public const string Applications = "/applications";
+    public const string NoVacancyUploadCv = "/upload-cv";
+    public const string Categories = "/categories";
+    public const string UserManager = "/userManager";
+    public const string Candidates = "/candidates";
+    public const string Users = "/Users";
+    public const string EmailTemplates = "/EmailTemplates";
 
     public static string GetPageFromUrl(string url, bool setDefault = false)
     {
